0b0b669a73dd981d599217a36627d34b77a68830
Nikolay Pozdnichenko <nick680@gmail.com>
2012-11-16 17:10:18 +0700

    Последняя версия распределений.

--- LibiadaWeb/Controllers/HomogeneousCalculationController.cs
+++ LibiadaWeb/Controllers/HomogeneousCalculationController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using LibiadaCore.Classes.Root;
 using LibiadaCore.Classes.Root.Characteristics;
