50e9f6fb72ca6e50c9be9689cd99217c56fb1e93
NickName <nick680@gmail.com>
2012-11-22 02:40:26 +0700

    обновил локальные характеристики, чтобы их можно было для нескольких цепочек сразу считать.

--- LibiadaWeb/Controllers/LocalCharacteristicsController.cs
+++ LibiadaWeb/Controllers/LocalCharacteristicsController.cs
@@ -49,135 +49,155 @@ namespace LibiadaWeb.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(long matterId, int[] characteristicIds, int[] linkUpIds, int notationId, int length,
+        public ActionResult Index(long[] matterIds, int[] characteristicIds, int[] linkUpIds, int notationId, int length,
                                   int step, bool isDelta, bool isSort, bool isFurie)
         {
 
-            List<List<Double>> characteristicsTemp = new List<List<Double>>();
-            String chainName = db.matter.Single(m => m.id == matterId).name;
-            List<String> partNames = new List<string>();
-            List<String> characteristicNames = new List<string>();
+            List<List<List<Double>>> characteristicsTemp = new List<List<List<Double>>>();
+            List<String> chainNames = new List<string>();
+            List<string> characteristicNames = new List<string>();
+            List<List<String>> partNames = new List<List<String>>();
 
 
+            for (int k = 0; k < matterIds.Length; k++)
+            {
+                long mattrId = matterIds[k];
+                chainNames.Add(db.matter.Single(m => m.id == mattrId).name);
+                partNames.Add(new List<string>());
+                characteristicsTemp.Add(new List<List<double>>());
+
+                matter matter = db.matter.Single(m => m.id == mattrId);
+                chain chain = matter.chain.Single(c => c.building_type_id == 1 && c.notation_id == notationId);
+                Chain libiadaChain = chainRepository.FromDbChainToLibiadaChain(chain);
+
+                for (int i = 0; i < characteristicIds.Length; i++)
+                {
+                    characteristicsTemp.Last().Add(new List<Double>());
+
+
+                    int characteristicId = characteristicIds[i];
+                    int linkUpId = linkUpIds[i];
+
+
+                    String className = db.characteristic_type.Single(c => c.id == characteristicId).class_name;
+                    ICharacteristicCalculator calculator = CharacteristicsFactory.Create(className);
+                    LinkUp linkUp = (LinkUp)db.link_up.Single(l => l.id == linkUpId).id;
+
+                    IteratorStart<Chain, Chain> iter = new IteratorStart<Chain, Chain>(libiadaChain, length, step);
+                    while (iter.Next())
+                    {
+                        Chain tempChain = iter.Current();
+                        partNames.Last().Add(tempChain.ToString());
+                        characteristicsTemp.Last().Last().Add(calculator.Calculate(tempChain, linkUp));
+                    }
+                }
+
+                if (isDelta)
+                {
+                    //Перебираем характеристики
+                    for (int i = 0; i < characteristicsTemp.Count; i++)
+                    {
+                        //перебираем фрагменты цепочек
+                        for (int j = (characteristicsTemp[i].Count) - 1; j > 0; j--)
+                        {
+                            characteristicsTemp.Last()[i][j] -= characteristicsTemp.Last()[i][j - 1];
+                        }
+                        characteristicsTemp.Last()[i].RemoveAt(0);
+                    }
+                }
+                if (isSort)
+                {
+                    //Перебираем характеристики
+                    for (int i = 0; i < characteristicsTemp.Count; i++)
+                    {
+                        //перебираем фрагменты цепочек
+                        characteristicsTemp.Last()[i].Sort();
+                    }
+                }
+
+                if (isFurie)
+                {
+
+                    //переводим в комлексный вид
+                    for (int i = 0; i < characteristicsTemp.Count; i++)
+                    {
+                        List<Complex> comp = new List<Complex>();
+                        int j = 0;
+
+                        for (j = 0; j < characteristicsTemp[i].Count; j++)
+                        {
+                            comp.Add(new Complex(characteristicsTemp.Last()[i][j], 0));
+                        }
+
+                        int m = 1;
+
+                        while (m < j)
+                        {
+                            m *= 2;
+                        }
+
+                        for (; j < m; j++)
+                        {
+                            comp.Add(new Complex(0, 0));
+                        }
+
+                        Complex[] data = FFT.Fft(comp.ToArray()); //вернёт массив
+                        List<double> temp = new List<double>();
+                        //переводим в массив double
+
+                        foreach (var fftElement in data)
+                        {
+                            temp.Add(fftElement.Real);
+                        }
+
+                        characteristicsTemp.Last()[i] = temp;
+                    }
+                }
+            }
+
+            List<List<List<Double>>> characteristics = new List<List<List<Double>>>();
+
+            //Перебираем цепочки
+            for (int z = 0; z < matterIds.Length; z++)
+            {
+                characteristics.Add(new List<List<double>>());
+                // перебираем характеристики
+                for (int t = 0; t < characteristicsTemp[z][0].Count; t++)
+                {
+                    characteristics[z].Add(new List<double>());
+                    //перебираем фрагменты
+                    for (int w = 0; w < characteristicsTemp[z].Count; w++)
+                    {
+                        characteristics[z][t].Add(characteristicsTemp[z][w][t]);
+                    }
+                }
+            }
 
             for (int i = 0; i < characteristicIds.Length; i++)
             {
-                characteristicsTemp.Add(new List<Double>());
-                matter matter = db.matter.Single(m => m.id == matterId);
-                chain chain = matter.chain.Single(c => c.building_type_id == 1 && c.notation_id == notationId);
-                Chain libiadaChain = chainRepository.FromDbChainToLibiadaChain(chain);
                 int characteristicId = characteristicIds[i];
                 int linkUpId = linkUpIds[i];
                 characteristicNames.Add(db.characteristic_type.Single(c => c.id == characteristicId).name + " " +
-                                        db.link_up.Single(l => l.id == linkUpId).name + " " +
-                                        db.notation.Single(n => n.id == notationId).name);
-
-                String className = db.characteristic_type.Single(c => c.id == characteristicId).class_name;
-                ICharacteristicCalculator calculator = CharacteristicsFactory.Create(className);
-                LinkUp linkUp = (LinkUp)db.link_up.Single(l => l.id == linkUpId).id;
-
-                IteratorStart<Chain, Chain> iter = new IteratorStart<Chain, Chain>(libiadaChain, length, step);
-                while (iter.Next())
-                {
-                    Chain tempChain = iter.Current();
-                    partNames.Add(tempChain.ToString());
-                    characteristicsTemp.Last().Add(calculator.Calculate(tempChain, linkUp));
-                }
+                                            db.link_up.Single(l => l.id == linkUpId).name);
             }
-
-            if (isDelta)
-            {
-                //Перебираем характеристики
-                for (int i = 0; i < characteristicsTemp.Count; i++)
-                {
-                    //перебираем фрагменты цепочек
-                    for (int j = (characteristicsTemp[i].Count) - 1; j > 0; j--)
-                    {
-                        characteristicsTemp[i][j] -= characteristicsTemp[i][j - 1];
-                    }
-                    characteristicsTemp[i].RemoveAt(0);
-                }
-            }
-            if (isSort)
-            {
-                //Перебираем характеристики
-                for (int i = 0; i < characteristicsTemp.Count; i++)
-                {
-                    //перебираем фрагменты цепочек
-                    characteristicsTemp[i].Sort();
-                }
-            }
-
-            if (isFurie)
-            {
-
-                //переводим в комлексный вид
-                for (int i = 0; i < characteristicsTemp.Count; i++)
-                {
-                    List<Complex> comp = new List<Complex>();
-                    int j = 0;
-
-                    for (j = 0; j < characteristicsTemp[i].Count; j++)
-                    {
-                        comp.Add(new Complex(characteristicsTemp[i][j], 0));
-                    }
-
-                    int k = 1;
-
-                    while (k < j)
-                    {
-                        k *= 2;
-                    }
-
-                    for (; j < k; j++)
-                    {
-                        comp.Add(new Complex(0, 0));
-                    }
-
-                    Complex[] data = FFT.Fft(comp.ToArray()); //вернёт массив
-                    List<double> temp = new List<double>();
-                    //переводим в массив double
-
-                    foreach (var fftElement in data)
-                    {
-                        temp.Add(fftElement.Real);
-                    }
-
-                    characteristicsTemp[i] = temp;
-                }
-            }
-
-
-            List<List<Double>> characteristics = new List<List<Double>>();
-
-            for (int t = 0; t < characteristicsTemp[0].Count; t++)
-            {
-                characteristics.Add(new List<double>());
-                for (int w = 0; w < characteristicsTemp.Count; w++)
-                {
-                    characteristics[t].Add(characteristicsTemp[w][t]);
-                }
-            }
-
-
 
             TempData["characteristics"] = characteristics;
-            TempData["chainName"] = chainName;
+            TempData["chainNames"] = chainNames;
             TempData["partNames"] = partNames;
-            TempData["characteristicNames"] = characteristicNames;
             TempData["characteristicIds"] = characteristicIds;
-            TempData["chainIds"] = matterId;
+            TempData["characteristicNames"] = characteristicNames;
+            TempData["chainIds"] = matterIds;
             return RedirectToAction("Result");
         }
 
         public ActionResult Result()
         {
-            List<List<double>> characteristics = TempData["characteristics"] as List<List<double>>;
-            List<String> characteristicNames = TempData["characteristicNames"] as List<String>;
+            List<List<List<double>>> characteristics = TempData["characteristics"] as List<List<List<double>>>;
             ViewBag.chainIds = TempData["chainIds"] as List<long>;
             int[] characteristicIds = TempData["characteristicIds"] as int[];
+            List<string> characteristicNames = TempData["characteristicNames"] as List<string>;
             List<SelectListItem> characteristicsList = new List<SelectListItem>();
-            for (int i = 0; i < characteristicNames.Count; i++)
+            for (int i = 0; i < characteristicIds.Length; i++)
             {
                 characteristicsList.Add(new SelectListItem
                 {
@@ -189,8 +209,8 @@ namespace LibiadaWeb.Controllers
             ViewBag.characteristicIds = new List<int>(characteristicIds);
             ViewBag.characteristicsList = characteristicsList;
             ViewBag.characteristics = characteristics;
-            ViewBag.chainName = TempData["chainName"] as String;
-            ViewBag.partNames = TempData["partNames"] as List<String>;
+            ViewBag.chainNames = TempData["chainNames"] as List<String>;
+            ViewBag.partNames = TempData["partNames"] as List<List<String>>;
             ViewBag.characteristicNames = characteristicNames;
             return View();
         }
