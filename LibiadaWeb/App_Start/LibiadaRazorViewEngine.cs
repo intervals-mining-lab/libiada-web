5bfb2712a01e3af08c2156dec46c0824ba664ff3
nickname670 <nick680@gmail.com>
2014-05-09 18:34:13 +0700

    Updated view engine routing.

--- LibiadaWeb/App_Start/LibiadaRazorViewEngine.cs
+++ LibiadaWeb/App_Start/LibiadaRazorViewEngine.cs
@@ -14,7 +14,8 @@ namespace LibiadaWeb
                     "~/Views/Shared/{0}.cshtml",
                     "~/Views/Chains/{1}/{0}.cshtml",
                     "~/Views/Calculators/{1}/{0}.cshtml",
-                    "~/Views/Catalogs/{1}/{0}.cshtml"
+                    "~/Views/Catalogs/{1}/{0}.cshtml",
+                    "~/Views/Characteristics/{1}/{0}.cshtml"
                 };
 
             PartialViewLocationFormats = ViewLocationFormats;
