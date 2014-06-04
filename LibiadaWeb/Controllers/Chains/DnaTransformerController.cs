5e5c7689af4aed578c0117788ad4dfe523038737
nickname670 <nick680@gmail.com>
2014-06-05 01:25:16 +0700

    Added dna transformer.

---
+++ LibiadaWeb/Controllers/Chains/DnaTransformerController.cs
@@ -0,0 +1,107 @@
+﻿namespace LibiadaWeb.Controllers.Chains
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using LibiadaCore.Core;
+    using LibiadaCore.Misc.DataTransformers;
+
+    using LibiadaWeb.Models;
+    using LibiadaWeb.Models.Repositories.Chains;
+
+    /// <summary>
+    /// The dna transformation controller.
+    /// </summary>
+    public class DnaTransformerController : Controller
+    {
+        /// <summary>
+        /// The db.
+        /// </summary>
+        private readonly LibiadaWebEntities db;
+
+        /// <summary>
+        /// The dna chain repository.
+        /// </summary>
+        private readonly DnaChainRepository dnaChainRepository;
+
+        /// <summary>
+        /// The chain repository.
+        /// </summary>
+        private readonly ChainRepository chainRepository;
+
+        /// <summary>
+        /// The element repository.
+        /// </summary>
+        private readonly ElementRepository elementRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DnaTransformerController"/> class.
+        /// </summary>
+        public DnaTransformerController()
+        {
+            db = new LibiadaWebEntities();
+            dnaChainRepository = new DnaChainRepository(db);
+            chainRepository = new ChainRepository(db);
+            elementRepository = new ElementRepository(db);
+        }
+
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Index()
+        {
+            var chains = db.dna_chain.Where(d => d.notation_id == 1 && d.dissimilar == false).Include("matter");
+            ViewBag.chains = chains.ToList();
+            ViewBag.chainsList = dnaChainRepository.GetSelectListItems(chains, null);
+            return View();
+        }
+
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <param name="chainIds">
+        /// The chain ids.
+        /// </param>
+        /// <param name="toAmino">
+        /// The to amino.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        public ActionResult Index(IEnumerable<long> chainIds, bool toAmino)
+        {
+            int notationId = toAmino ? Aliases.NotationAminoAcid : Aliases.NotationTriplet;
+
+            foreach (var chainId in chainIds)
+            {
+                chain dataBaseChain = db.chain.Single(c => c.id == chainId);
+                Chain sourceChain = chainRepository.ToLibiadaChain(chainId);
+
+                BaseChain transformedChain = toAmino
+                                                 ? DnaTransformer.Encode(sourceChain)
+                                                 : DnaTransformer.EncodeTriplets(sourceChain);
+
+                var result = new dna_chain
+                    {
+                        matter_id = dataBaseChain.matter_id,
+                        dissimilar = false,
+                        notation_id = notationId,
+                        created = DateTime.Now,
+                        piece_type_id = Aliases.PieceTypeFullGenome,
+                        piece_position = 0
+                    };
+                long[] alphabet = elementRepository.ToDbElements(transformedChain.Alphabet, notationId, false);
+                dnaChainRepository.Insert(result, alphabet, transformedChain.Building);
+            }
+
+            return RedirectToAction("Index", "Chain");
+        }
+    }
+}
