cf1bc32e7e68a2ec2100c712c5abe4ee433834e0
nickname670 <nick680@gmail.com>
2014-06-17 13:51:33 +0700

    Draft of genes calculation.

--- LibiadaWeb/Controllers/Calculators/GenesCalculationController.cs
+++ LibiadaWeb/Controllers/Calculators/GenesCalculationController.cs
@@ -121,73 +121,44 @@
         /// <param name="notationIds">
         /// The notation ids.
         /// </param>
-        /// <param name="languageIds">
-        /// The language ids.
-        /// </param>
-        /// <param name="translatorIds">
-        /// The translator ids.
+        /// <param name="pieceTypeIds">
+        /// The piece type ids.
         /// </param>
         /// <param name="isSort">
         /// The is sort.
-        /// </param>
-        /// <param name="theoretical">
-        /// The theoretical.
         /// </param>
         /// <returns>
         /// The <see cref="ActionResult"/>.
         /// </returns>
         [HttpPost]
         public ActionResult Index(
-            long[] matterIds, 
-            int[] characteristicIds, 
-            int[] linkIds, 
-            int[] notationIds, 
-            int[] languageIds, 
-            int?[] translatorIds, 
-            bool isSort, 
-            bool theoretical)
+            long[] matterIds,
+            int[] characteristicIds,
+            int[] linkIds,
+            int[] notationIds,
+            int[] pieceTypeIds,
+            bool isSort)
         {
             var characteristics = new List<List<List<KeyValuePair<int, double>>>>();
-            var teoreticalRanks = new List<List<List<double>>>();
             var chainNames = new List<string>();
-            var elementNames = new List<List<string>>();
+            var chainsProduct = new List<List<string>>();
             var characteristicNames = new List<string>();
-
-            bool isLiteratureChain = false;
 
             // Перебор всех цепочек; первый уровень массива характеристик
             for (int w = 0; w < matterIds.Length; w++)
             {
                 long matterId = matterIds[w];
                 chainNames.Add(db.matter.Single(m => m.id == matterId).name);
-                elementNames.Add(new List<string>());
+                chainsProduct.Add(new List<string>());
                 characteristics.Add(new List<List<KeyValuePair<int, double>>>());
-                teoreticalRanks.Add(new List<List<double>>());
+
+                var chains = db.dna_chain.Where(c => c.matter_id == matterId &&
+                        pieceTypeIds.Contains(c.piece_type_id) &&
+                        notationIds.Contains(c.notation_id)).ToArray();
 
                 // Перебор всех характеристик и форм записи; второй уровень массива характеристик
                 for (int i = 0; i < characteristicIds.Length; i++)
                 {
-                    int notationId = notationIds[i];
-                    int languageId = languageIds[i];
-                    int? translatorId = translatorIds[i];
-                    long chainId;
-
-                    if (db.matter.Single(m => m.id == matterId).nature_id == 3)
-                    {
-                        isLiteratureChain = true;
-                        chainId = db.literature_chain.Single(l => l.matter_id == matterId &&
-                                    l.notation_id == notationId
-                                    && l.language_id == languageId
-                                    && ((translatorId == null && l.translator_id == null)
-                                                    || (translatorId == l.translator_id))).id;
-                    }
-                    else
-                    {
-                        chainId = db.chain.Single(c => c.matter_id == matterId && c.notation_id == notationId).id;
-                    }
-
-                    Chain libiadaChain = this.chainRepository.ToLibiadaChain(chainId);
-                    libiadaChain.FillIntervalManagers();
                     characteristics.Last().Add(new List<KeyValuePair<int, double>>());
                     int characteristicId = characteristicIds[i];
                     int linkId = linkIds[i];
@@ -195,50 +166,39 @@
                     string className = db.characteristic_type.Single(c => c.id == characteristicId).class_name;
                     ICalculator calculator = CalculatorsFactory.Create(className);
                     Link link = (Link)db.link.Single(l => l.id == linkId).id;
-                    List<long> chainElements = this.chainRepository.GetElementIds(chainId);
-                    int calculated = db.congeneric_characteristic.Count(b => b.chain_id == chainId &&
-                                                                              b.characteristic_type_id == characteristicId &&
-                                                                              b.link_id == linkId);
-                    if (calculated < libiadaChain.Alphabet.Cardinality)
+
+                    for (int j = 0; j < chains.Length; j++)
                     {
+                        long chainId = chains[j].id;
 
-                        for (int j = 0; j < libiadaChain.Alphabet.Cardinality; j++)
+                        Chain libiadaChain = chainRepository.ToLibiadaChain(chainId);
+                        libiadaChain.FillIntervalManagers();
+
+                        if (!db.characteristic.Any(b =>
+                                                        b.chain_id == chainId &&
+                                                        b.characteristic_type_id == characteristicId &&
+                                                        b.link_id == linkId))
                         {
-                            long elementId = chainElements[j];
-
-                            CongenericChain tempChain = libiadaChain.CongenericChain(j);
-
-                            if (!db.congeneric_characteristic.Any(b =>
-                                                                   b.chain_id == chainId &&
-                                                                   b.characteristic_type_id == characteristicId &&
-                                                                   b.element_id == elementId && b.link_id == linkId))
+                            double value = calculator.Calculate(libiadaChain, link);
+                            var currentCharacteristic = new characteristic
                             {
-                                double value = calculator.Calculate(tempChain, link);
-                                var currentCharacteristic = new congeneric_characteristic
-                                {
-                                    chain_id = chainId, 
-                                    characteristic_type_id = characteristicId, 
-                                    link_id = linkId, 
-                                    element_id = elementId, 
-                                    value = value, 
-                                    value_string = value.ToString(), 
-                                    created = DateTime.Now
-                                };
-                                db.congeneric_characteristic.Add(currentCharacteristic);
-                                db.SaveChanges();
-                            }
+                                chain_id = chainId,
+                                characteristic_type_id = characteristicId,
+                                link_id = linkId,
+                                value = value,
+                                value_string = value.ToString()
+                            };
+                            db.characteristic.Add(currentCharacteristic);
+                            db.SaveChanges();
                         }
                     }
 
-                    // Перебор всех элементов алфавита; третий уровень массива характеристик
-                    for (int d = 0; d < libiadaChain.Alphabet.Cardinality; d++)
+                    for (int d = 0; d < chains.Length; d++)
                     {
-                        long elementId = chainElements[d];
-
+                        long chainId = chains[d].id;
                         double? characteristic = db.congeneric_characteristic.Single(b =>
                                     b.chain_id == chainId &&
                                     b.characteristic_type_id == characteristicId &&
-                                    b.element_id == elementId &&
                                     b.link_id == linkId).value;
 
                         characteristics.Last().Last().Add(
@@ -246,61 +206,19 @@
 
                         if (i == 0)
                         {
-                            elementNames.Last().Add(libiadaChain.Alphabet[d].ToString());
-                        }
-                    }
-
-                    // теоретические частоты по критерию Орлова
-                    if (theoretical)
-                    {
-
-                        teoreticalRanks[w].Add(new List<double>());
-                        ICalculator countCalculator = CalculatorsFactory.Create("Count");
-                        var counts = new List<int>();
-                        for (int f = 0; f < libiadaChain.Alphabet.Cardinality; f++)
-                        {
-                            counts.Add((int)countCalculator.Calculate(libiadaChain.CongenericChain(f), Link.End));
-                        }
-
-                        ICalculator frequencyCalculator = CalculatorsFactory.Create("Probability");
-                        var frequency = new List<double>();
-                        for (int f = 0; f < libiadaChain.Alphabet.Cardinality; f++)
-                        {
-                            frequency.Add(frequencyCalculator.Calculate(libiadaChain.CongenericChain(f), Link.End));
-                        }
-
-                        double maxFrequency = frequency.Max();
-                        double K = 1 / Math.Log(counts.Max());
-                        double B = (K / maxFrequency) - 1;
-                        int n = 1;
-                        double Plow = libiadaChain.Length;
-                        double P = K / (B + n);
-                        while (P >= (1 / Plow))
-                        {
-                            teoreticalRanks.Last().Last().Add(P);
-                            n++;
-                            P = K / (B + n);
+                            chainsProduct.Last().Add(db.product.Single(p => chains[d].product_id == p.id).name);
                         }
                     }
                 }
-
             }
 
             // подписи для характеристик
             for (int k = 0; k < characteristicIds.Length; k++)
             {
                 int characteristicId = characteristicIds[k];
-                int languageId = languageIds[k];
+
                 string characteristicType = db.characteristic_type.Single(c => c.id == characteristicId).name;
-                if (isLiteratureChain)
-                {
-                    string language = db.language.Single(l => l.id == languageId).name;
-                    characteristicNames.Add(characteristicType + " " + language);
-                }
-                else
-                {
-                    characteristicNames.Add(characteristicType);
-                }
+                characteristicNames.Add(characteristicType);
             }
 
             // ранговая сортировка
@@ -310,7 +228,7 @@
                 {
                     for (int p = 0; p < characteristics[f].Count; p++)
                     {
-                        this.SortKeyValuePairList(characteristics[f][p]);
+                        SortKeyValuePairList(characteristics[f][p]);
                     }
                 }
             }
@@ -319,10 +237,9 @@
                                      {
                                          { "characteristics", characteristics }, 
                                          { "chainNames", chainNames }, 
-                                         { "elementNames", elementNames }, 
+                                         { "chainsProduct", chainsProduct }, 
                                          { "characteristicNames", characteristicNames }, 
-                                         { "matterIds", matterIds }, 
-                                         { "teoreticalRanks", teoreticalRanks }
+                                         { "matterIds", matterIds }
                                      };
 
             return this.RedirectToAction("Result");
@@ -351,7 +268,7 @@
         {
             try
             {
-                var result = this.TempData["characteristics"] as Dictionary<string, object>;
+                var result = this.TempData["result"] as Dictionary<string, object>;
                 if (result == null)
                 {
                     throw new Exception("Нет данных для отображения");
@@ -363,8 +280,8 @@
                 {
                     characteristicsList.Add(new SelectListItem
                     {
-                        Value = i.ToString(), 
-                        Text = characteristicNames[i], 
+                        Value = i.ToString(),
+                        Text = characteristicNames[i],
                         Selected = false
                     });
                 }
@@ -373,15 +290,14 @@
                 ViewBag.characteristicsList = characteristicsList;
                 ViewBag.characteristics = result["characteristics"];
                 ViewBag.chainNames = result["chainNames"];
-                ViewBag.elementNames = result["elementNames"];
+                ViewBag.chainsProduct = result["chainsProduct"];
                 ViewBag.characteristicNames = characteristicNames;
-                ViewBag.theoreticalRanks = result["teoreticalRanks"];
 
-                this.TempData.Keep();
+                TempData.Keep();
             }
             catch (Exception e)
             {
-                this.ModelState.AddModelError("Error", e.Message);
+                ModelState.AddModelError("Error", e.Message);
             }
 
             return View();
