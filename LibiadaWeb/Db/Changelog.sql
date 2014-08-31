026573aef4b2559e4bf8a3db57effcd0d196d741
NickName <nick680@gmail.com>
2014-09-01 02:33:35 +0700

    Characteristics renaming.

--- LibiadaWeb/Db/Changelog.sql
+++ LibiadaWeb/Db/Changelog.sql
@@ -1,3 +1,13 @@
 BEGIN;
 
+-- 31.09.2014
+-- Добавлены и переименованы некоторые характеристики.
+
+UPDATE characteristic_type SET class_name = 'AverageRemotenessDispersion' WHERE id = 28;
+UPDATE characteristic_type SET class_name = 'AverageRemotenessStandardDeviation' WHERE id = 29;
+UPDATE characteristic_type SET class_name = 'AverageRemotenessSkewness' WHERE id = 30;
+
+INSERT INTO characteristic_type (name, description, characteristic_group_id, class_name, linkable, full_chain_applicable, congeneric_chain_applicable, binary_chain_applicable) VALUES ('Нормированная ассиметрия средних удаленностей', 'коэффициент ассиметрии (скошенность) распределения средних удаленностей однородных цепей', NULL, 'NormalizedAverageRemotenessSkewness', true, true, false, false);
+INSERT INTO characteristic_type (name, description, characteristic_group_id, class_name, linkable, full_chain_applicable, congeneric_chain_applicable, binary_chain_applicable) VALUES ('Коэффициент соответствия', 'Коэффициент соответствия двух однородных цепей друг другу', NULL, 'ComplianceDegree', false, false, false, true);
+
 COMMIT;
