1e96890dd916169fcd278c72a9f77adf403ddaea
NickName <nick680@gmail.com>
2014-09-06 21:46:38 +0700

    Added some indexes on genes.

--- LibiadaWeb/Db/Changelog.sql
+++ LibiadaWeb/Db/Changelog.sql
@@ -10,4 +10,10 @@ UPDATE characteristic_type SET class_name = 'AverageRemotenessSkewness' WHERE id
 INSERT INTO characteristic_type (name, description, characteristic_group_id, class_name, linkable, full_chain_applicable, congeneric_chain_applicable, binary_chain_applicable) VALUES ('Нормированная ассиметрия средних удаленностей', 'коэффициент ассиметрии (скошенность) распределения средних удаленностей однородных цепей', NULL, 'NormalizedAverageRemotenessSkewness', true, true, false, false);
 INSERT INTO characteristic_type (name, description, characteristic_group_id, class_name, linkable, full_chain_applicable, congeneric_chain_applicable, binary_chain_applicable) VALUES ('Коэффициент соответствия', 'Коэффициент соответствия двух однородных цепей друг другу', NULL, 'ComplianceDegree', false, false, false, true);
 
+-- 06.09.2014
+-- Добавлены индексы на гены.
+
+CREATE INDEX ix_piece_id ON piece (id ASC NULLS LAST);
+CREATE INDEX ix_piece_gene_id ON piece (gene_id ASC NULLS LAST);
+
 COMMIT;
