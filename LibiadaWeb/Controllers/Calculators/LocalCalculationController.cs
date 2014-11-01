504929dac59a24510f929ef126362c362a6b70bf
NickName <nick680@gmail.com>
2014-11-01 20:29:18 +0600

    Refactoring in local characteristics calculator.
    Also updated npgsql to 2.2.1.0.

--- LibiadaWeb/Controllers/Calculators/LocalCalculationController.cs
+++ LibiadaWeb/Controllers/Calculators/LocalCalculationController.cs
@@ -112,15 +112,6 @@
         /// <param name="step">
         /// The step.
         /// </param>
-        /// <param name="startCoordinate">
-        /// The start coordinate.
-        /// </param>
-        /// <param name="beginOfChain">
-        /// The begin of chain.
-        /// </param>
-        /// <param name="endOfChain">
-        /// The end of chain.
-        /// </param>
         /// <param name="isDelta">
         /// The is delta.
         /// </param>
@@ -129,12 +120,6 @@
         /// </param>
         /// <param name="isGrowingWindow">
         /// The is growing window.
-        /// </param>
-        /// <param name="isMoveCoordinate">
-        /// The is move coordinate.
-        /// </param>
-        /// <param name="isSetBeginAndEnd">
-        /// The is set begin and end.
         /// </param>
         /// <param name="isAutocorrelation">
         /// The is autocorelation.
@@ -150,39 +135,35 @@
             int languageId, 
             int notationId, 
             int length, 
-            int step, 
-            int startCoordinate, 
-            int beginOfChain, 
-            int endOfChain, 
-            bool isDelta, 
-            bool isFurie, 
-            bool isGrowingWindow, 
-            bool isMoveCoordinate, 
-            bool isSetBeginAndEnd, 
+            int step,
+            bool isDelta,
+            bool isFourier, 
+            bool isGrowingWindow,
             bool isAutocorrelation)
         {
             List<List<List<double>>> characteristics = CalculateCharacteristics(
-                matterIds, 
-                isSetBeginAndEnd, 
+                matterIds,
                 isGrowingWindow, 
                 notationId, 
                 languageId, 
                 length, 
                 characteristicIds, 
                 linkIds, 
-                step, 
-                beginOfChain, 
-                endOfChain);
+                step);
 
             var chainNames = new List<string>();
             var characteristicNames = new List<string>();
             var partNames = new List<List<string>>();
+            var starts = new List<List<int>>();
+            var lengthes = new List<List<int>>();
 
             for (int k = 0; k < matterIds.Length; k++)
             {
                 long matterId = matterIds[k];
                 chainNames.Add(db.matter.Single(m => m.id == matterId).name);
                 partNames.Add(new List<string>());
+                starts.Add(new List<int>());
+                lengthes.Add(new List<int>());
 
                 long chainId;
                 if (db.matter.Single(m => m.id == matterId).nature_id == 3)
@@ -199,31 +180,15 @@
 
                 Chain libiadaChain = this.chainRepository.ToLibiadaChain(chainId);
 
-                CutRule cutRule;
-                if (isSetBeginAndEnd)
-                {
-                    cutRule = isGrowingWindow
-                                  ? (CutRule)
-                                    new CutRuleWithShiftedAndFixedStart(endOfChain - beginOfChain, step, beginOfChain)
-                                  : new SimpleCutRuleWithShiftedStart(
-                                        endOfChain - beginOfChain, 
-                                        step, 
-                                        length, 
-                                        beginOfChain);
-                }
-                else
-                {
-                    cutRule = isGrowingWindow
+                CutRule cutRule = isGrowingWindow
                                   ? (CutRule)new CutRuleWithFixedStart(libiadaChain.GetLength(), step)
                                   : new SimpleCutRule(libiadaChain.GetLength(), step, length);
-                }
 
                 CutRuleIterator iter = cutRule.GetIterator();
 
                 while (iter.Next())
                 {
-                    var tempChain = new Chain();
-                    tempChain.ClearAndSetNewLength(iter.GetEndPosition() - iter.GetStartPosition());
+                    var tempChain = new Chain(iter.GetEndPosition() - iter.GetStartPosition());
 
                     for (int i = 0; iter.GetStartPosition() + i < iter.GetEndPosition(); i++)
                     {
@@ -231,117 +196,31 @@
                     }
 
                     partNames.Last().Add(tempChain.ToString());
-                }
-
-                if (isMoveCoordinate)
-                {
-                    List<List<List<double>>> characteristicsParts = this.CalculateCharacteristics(
-                        matterIds, 
-                        isSetBeginAndEnd, 
-                        isGrowingWindow, 
-                        notationId, 
-                        languageId, 
-                        startCoordinate, 
-                        characteristicIds, 
-                        linkIds, 
-                        step, 
-                        beginOfChain, 
-                        endOfChain);
-                    for (int i = 0; i < characteristics.Count; i++)
-                    {
-                        for (int j = 0; j < characteristics[i].Count; j++)
-                        {
-                            for (int l = 0; l < characteristics[i][j].Count; l++)
-                            {
-                                characteristics[i][j][l] -= characteristicsParts[i][j][l];
-                            }
-                        }
-                    }
+                    starts.Last().Add(iter.GetStartPosition());
+                    lengthes.Last().Add(tempChain.GetLength());
                 }
 
                 if (isDelta)
                 {
-                    // Перебираем характеристики 
-                    for (int i = 0; i < characteristics.Last().Last().Count; i++)
-                    {
-                        // перебираем фрагменты цепочек
-                        for (int j = characteristics.Last().Count - 1; j > 0; j--)
-                        {
-                            characteristics.Last()[j][i] -= characteristics.Last()[j - 1][i];
-                        }
-                    }
-
-                    characteristics.Last().RemoveAt(0);
+                    CalculateDelta(characteristics);
                 }
 
-                if (isFurie)
+                if (isFourier)
                 {
-                    // переводим в комлексный вид
-                    // Для всех характеристик
-                    for (int i = 0; i < characteristics.Last().Last().Count; i++)
-                    {
-                        var complex = new List<Complex>();
-                        int j;
-
-                        // Для всех фрагментов цепочек
-                        for (j = 0; j < characteristics.Last().Count; j++)
-                        {
-                            complex.Add(new Complex(characteristics.Last()[j][i], 0));
-                        }
-
-                        int m = 1;
-
-                        while (m < j)
-                        {
-                            m *= 2;
-                        }
-
-                        for (; j < m; j++)
-                        {
-                            complex.Add(new Complex(0, 0));
-                        }
-
-                        Complex[] data = FFT.Fft(complex.ToArray()); // вернёт массив
-
-                        // переводим в массив double
-                        for (int g = 0; g < characteristics.Last().Count; g++)
-                        {
-                            characteristics.Last()[g][i] = data[g].Real;
-                        }
-                    }
+                    FourierTransform(characteristics);
                 }
             }
 
             if (isAutocorrelation)
             {
-                var autoCorrellation = new AutoCorrelation();
-                for (int i = 0; i < characteristics.Last().Last().Count; i++)
-                {
-                    var temp = new double[characteristics.Last().Count];
-
-                    // Для всех фрагментов цепочек
-                    for (int j = 0; j < characteristics.Last().Count; j++)
-                    {
-                        temp[j] = characteristics.Last()[j][i];
-                    }
-
-                    double[] res = autoCorrellation.Execute(temp);
-                    for (int j = 0; j < res.Length; j++)
-                    {
-                        characteristics.Last()[j][i] = res[j];
-                    }
-
-                    characteristics.Last().RemoveRange(res.Length, characteristics.Last().Count - res.Length);
-                }
+                CalculateAutocorrelation(characteristics);
             }
 
             for (int i = 0; i < characteristicIds.Length; i++)
             {
                 int characteristicId = characteristicIds[i];
                 int linkId = linkIds[i];
-                characteristicNames.Add(
-                    db.characteristic_type.Single(c => c.id == characteristicId).name + " "
-                    + db.link.Single(l => l.id == linkId).name);
+                characteristicNames.Add(db.characteristic_type.Single(c => c.id == characteristicId).name + " " + db.link.Single(l => l.id == linkId).name);
             }
 
             var characteristicsList = new List<SelectListItem>();
@@ -359,7 +238,9 @@
                                      {
                                         { "characteristics", characteristics },
                                         { "chainNames", chainNames },
-                                        { "partNames", partNames },
+                                        //{ "partNames", partNames },
+                                        { "starts", starts },
+                                        { "lengthes", lengthes },
                                         { "characteristicIds", new List<int>(characteristicIds) },
                                         { "characteristicNames", characteristicNames },
                                         { "chainIds", matterIds },
@@ -408,13 +289,103 @@
         }
 
         /// <summary>
+        /// The calculate autocorrelation.
+        /// </summary>
+        /// <param name="characteristics">
+        /// The characteristics.
+        /// </param>
+        private void CalculateAutocorrelation(List<List<List<double>>> characteristics)
+        {
+            var autoCorrelation = new AutoCorrelation();
+            for (int i = 0; i < characteristics.Last().Last().Count; i++)
+            {
+                var temp = new double[characteristics.Last().Count];
+
+                // Для всех фрагментов цепочек
+                for (int j = 0; j < characteristics.Last().Count; j++)
+                {
+                    temp[j] = characteristics.Last()[j][i];
+                }
+
+                double[] res = autoCorrelation.Execute(temp);
+                for (int j = 0; j < res.Length; j++)
+                {
+                    characteristics.Last()[j][i] = res[j];
+                }
+
+                characteristics.Last().RemoveRange(res.Length, characteristics.Last().Count - res.Length);
+            }
+        }
+
+        /// <summary>
+        /// The calculate delta.
+        /// </summary>
+        /// <param name="characteristics">
+        /// The characteristics.
+        /// </param>
+        private void CalculateDelta(List<List<List<double>>> characteristics)
+        {
+            // Перебираем характеристики 
+            for (int i = 0; i < characteristics.Last().Last().Count; i++)
+            {
+                // перебираем фрагменты цепочек
+                for (int j = characteristics.Last().Count - 1; j > 0; j--)
+                {
+                    characteristics.Last()[j][i] -= characteristics.Last()[j - 1][i];
+                }
+            }
+
+            characteristics.Last().RemoveAt(0);
+        }
+
+        /// <summary>
+        /// The fourier transform.
+        /// </summary>
+        /// <param name="characteristics">
+        /// The characteristics.
+        /// </param>
+        private void FourierTransform(List<List<List<double>>> characteristics)
+        {
+            // переводим в комлексный вид
+            // Для всех характеристик
+            for (int i = 0; i < characteristics.Last().Last().Count; i++)
+            {
+                var complex = new List<Complex>();
+                int j;
+
+                // Для всех фрагментов цепочек
+                for (j = 0; j < characteristics.Last().Count; j++)
+                {
+                    complex.Add(new Complex(characteristics.Last()[j][i], 0));
+                }
+
+                int m = 1;
+
+                while (m < j)
+                {
+                    m *= 2;
+                }
+
+                for (; j < m; j++)
+                {
+                    complex.Add(new Complex(0, 0));
+                }
+
+                Complex[] data = FFT.Fft(complex.ToArray()); // вернёт массив
+
+                // переводим в массив double
+                for (int g = 0; g < characteristics.Last().Count; g++)
+                {
+                    characteristics.Last()[g][i] = data[g].Real;
+                }
+            }
+        }
+
+        /// <summary>
         /// The calculate characteristics.
         /// </summary>
         /// <param name="matterIds">
         /// The matter ids.
-        /// </param>
-        /// <param name="isSetBeginAndEnd">
-        /// The is set begin and end.
         /// </param>
         /// <param name="isGrowingWindow">
         /// The is growing window.
@@ -437,27 +408,18 @@
         /// <param name="step">
         /// The step.
         /// </param>
-        /// <param name="beginOfChain">
-        /// The begin of chain.
-        /// </param>
-        /// <param name="endOfChain">
-        /// The end of chain.
-        /// </param>
         /// <returns>
         /// The <see cref="List"/>.
         /// </returns>
         private List<List<List<double>>> CalculateCharacteristics(
             long[] matterIds,
-            bool isSetBeginAndEnd,
             bool isGrowingWindow,
             int notationId,
             int languageId,
             int length,
             int[] characteristicIds,
             int[] linkIds,
-            int step,
-            int beginOfChain,
-            int endOfChain)
+            int step)
         {
             var characteristics = new List<List<List<double>>>();
             for (int k = 0; k < matterIds.Length; k++)
@@ -468,10 +430,9 @@
                 long chainId;
                 if (db.matter.Single(m => m.id == matterId).nature_id == 3)
                 {
-                    chainId =
-                        db.literature_chain.Single(
-                            l => l.matter_id == matterId && l.notation_id == notationId && l.language_id == languageId)
-                            .id;
+                    chainId = db.literature_chain.Single(l => l.matter_id == matterId && 
+                                                              l.notation_id == notationId && 
+                                                              l.language_id == languageId).id;
                 }
                 else
                 {
@@ -481,23 +442,11 @@
                 Chain libiadaChain = this.chainRepository.ToLibiadaChain(chainId);
 
                 CutRule cutRule;
-                if (isSetBeginAndEnd)
-                {
-                    cutRule = isGrowingWindow
-                                  ? (CutRule)
-                                    new CutRuleWithShiftedAndFixedStart(endOfChain - beginOfChain, step, beginOfChain)
-                                  : new SimpleCutRuleWithShiftedStart(
-                                        endOfChain - beginOfChain,
-                                        step,
-                                        length,
-                                        beginOfChain);
-                }
-                else
-                {
-                    cutRule = isGrowingWindow
-                                  ? (CutRule)new CutRuleWithFixedStart(libiadaChain.GetLength(), step)
-                                  : new SimpleCutRule(libiadaChain.GetLength(), step, length);
-                }
+
+
+                cutRule = isGrowingWindow
+                                ? (CutRule)new CutRuleWithFixedStart(libiadaChain.GetLength(), step)
+                                : new SimpleCutRule(libiadaChain.GetLength(), step, length);
 
                 CutRuleIterator iter = cutRule.GetIterator();
 
