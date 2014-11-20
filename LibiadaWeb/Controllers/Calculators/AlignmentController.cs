f52d19d14d3696d6b4c638be7661692ea27892e0
NickName670 <nick680@gmail.com>
2014-11-20 18:19:08 +0600

    Draft of genes alignment.
    Also libraries update.

---
+++ LibiadaWeb/Controllers/Calculators/AlignmentController.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using LibiadaCore.Core;
+using LibiadaCore.Core.Characteristics;
+using LibiadaCore.Core.Characteristics.Calculators;
+using LibiadaCore.Misc.Iterators;
+using LibiadaWeb.Helpers;
+using LibiadaWeb.Models;
+using LibiadaWeb.Models.Repositories.Catalogs;
+using LibiadaWeb.Models.Repositories.Chains;
+
+namespace LibiadaWeb.Controllers.Calculators
+{
+    public class AlignmentController : Controller
+    {
+        /// <summary>
+        /// The db.
+        /// </summary>
+        private readonly LibiadaWebEntities db;
+
+        /// <summary>
+        /// The matter repository.
+        /// </summary>
+        private readonly MatterRepository matterRepository;
+
+        /// <summary>
+        /// The characteristic repository.
+        /// </summary>
+        private readonly CharacteristicTypeRepository characteristicRepository;
+
+        /// <summary>
+        /// The piece type repository.
+        /// </summary>
+        private readonly PieceTypeRepository pieceTypeRepository;
+
+        /// <summary>
+        /// The chain repository.
+        /// </summary>
+        private readonly ChainRepository chainRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlignmentController"/> class.
+        /// </summary>
+        public AlignmentController()
+        {
+            db = new LibiadaWebEntities();
+            this.matterRepository = new MatterRepository(db);
+            this.characteristicRepository = new CharacteristicTypeRepository(db);
+            this.pieceTypeRepository = new PieceTypeRepository(db);
+            this.chainRepository = new ChainRepository(db);
+        }
+
+        // GET: Alignment
+        public ActionResult Index()
+        {
+            ViewBag.dbName = DbHelper.GetDbName(db);
+
+            var chainIds = db.gene.Select(g => g.chain_id).Distinct().ToList();
+            var chains = db.dna_chain.Where(c => chainIds.Contains(c.id));
+            var matterIds = chains.Select(c => c.matter_id);
+
+            List<matter> matters = db.matter.Where(m => matterIds.Contains(m.id)).ToList();
+
+            IEnumerable<characteristic_type> characteristicsList = db.characteristic_type.Where(c => c.full_chain_applicable);
+
+            var characteristicTypes = characteristicRepository.GetSelectListWithLinkable(characteristicsList);
+
+            var pieceTypeIds =
+                db.piece_type.Where(p => p.nature_id == Aliases.NatureGenetic
+                                         && p.id != Aliases.PieceTypeFullGenome
+                                         && p.id != Aliases.PieceTypeChloroplastGenome
+                                         && p.id != Aliases.PieceTypeMitochondrionGenome).Select(p => p.id);
+
+            var links = new SelectList(db.link, "id", "name").ToList();
+            links.Insert(0, new SelectListItem { Value = null, Text = "Нет" });
+
+            ViewBag.data = new Dictionary<string, object>
+                {
+                    { "matters", new SelectList(matters, "id", "name") }, 
+                    { "characteristicTypes", characteristicTypes }, 
+                    { "notations", new SelectList(db.notation.Where(n => n.nature_id == Aliases.NatureGenetic), "id", "name") }, 
+                    { "links", links }, 
+                    { "matterCheckBoxes", matterRepository.GetSelectListItems(matters, null) }, 
+                    { "pieceTypesCheckBoxes", pieceTypeRepository.GetSelectListWithNature(pieceTypeIds, pieceTypeIds) }
+                };
+            return View();
+        }
+
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <param name="matterIds">
+        /// The matter ids.
+        /// </param>
+        /// <param name="characteristicIds">
+        /// The characteristic ids.
+        /// </param>
+        /// <param name="linkIds">
+        /// The link ids.
+        /// </param>
+        /// <param name="notationIds">
+        /// The notation ids.
+        /// </param>
+        /// <param name="pieceTypeIds">
+        /// The piece type ids.
+        /// </param>
+        /// <param name="isSort">
+        /// The is sort.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        public ActionResult Index(
+            long[] matterIds,
+            int characteristicId,
+            int? linkId,
+            int notationId,
+            int[] pieceTypeIds)
+        {
+            var characteristics = new List<List<KeyValuePair<int, double>>>();
+            var chainNames = new List<string>();
+            var chainsProduct = new List<List<string>>();
+            var chainsPosition = new List<List<long>>();
+            var chainsPieceTypes = new List<List<string>>();
+            var characteristicNames = new List<string>();
+
+            // Перебор всех цепочек; первый уровень массива характеристик
+            for (int w = 0; w < matterIds.Length; w++)
+            {
+                long matterId = matterIds[w];
+                chainNames.Add(db.matter.Single(m => m.id == matterId).name);
+                chainsProduct.Add(new List<string>());
+                chainsPosition.Add(new List<long>());
+                chainsPieceTypes.Add(new List<string>());
+                characteristics.Add(new List<KeyValuePair<int, double>>());
+
+                var chainId = db.dna_chain.Single(c => c.matter_id == matterId && c.notation_id == notationId).id;
+
+                var genes = db.gene.Where(g => g.chain_id == chainId && pieceTypeIds.Contains(g.piece_type_id)).Include("piece").ToArray();
+
+                var pieces = genes.Select(g => g.piece.First()).ToList();
+
+                var chains = ExtractChains(pieces, chainId);
+
+                // Перебор всех характеристик и форм записи; второй уровень массива характеристик
+
+                string className = db.characteristic_type.Single(c => c.id == characteristicId).class_name;
+                IFullCalculator calculator = CalculatorsFactory.CreateFullCalculator(className);
+                var link = linkId != null ? (Link)db.link.Single(l => l.id == linkId).id : Link.None;
+
+                for (int j = 0; j < chains.Count; j++)
+                {
+                    long geneId = genes[j].id;
+
+                    if (!db.characteristic.Any(c => c.chain_id == geneId &&
+                                                    c.characteristic_type_id == characteristicId &&
+                                                    ((linkId == null && c.link_id == null) || (linkId == c.link_id))))
+                    {
+                        double value = calculator.Calculate(chains[j], link);
+                        var currentCharacteristic = new characteristic
+                        {
+                            chain_id = geneId,
+                            characteristic_type_id = characteristicId,
+                            link_id = linkId,
+                            value = value,
+                            value_string = value.ToString()
+                        };
+
+                        db.characteristic.Add(currentCharacteristic);
+                        db.SaveChanges();
+                    }
+                }
+
+                for (int d = 0; d < chains.Count; d++)
+                {
+                    long geneId = genes[d].id;
+                    double? characteristic = db.characteristic.Single(c =>
+                                c.chain_id == geneId &&
+                                c.characteristic_type_id == characteristicId &&
+                                ((linkId == null && c.link_id == null) || (linkId == c.link_id))).value;
+
+                    characteristics.Last().Add(new KeyValuePair<int, double>(d, (double)characteristic));
+
+
+                    var productId = genes[d].product_id;
+                    var pieceTypeId = genes[d].piece_type_id;
+
+                    chainsProduct.Last().Add(productId == null ? string.Empty : db.product.Single(p => productId == p.id).name);
+                    chainsPosition.Last().Add(pieces[d].start);
+
+                    chainsPieceTypes.Last().Add(db.piece_type.Single(p => pieceTypeId == p.id).name);
+                }
+            }
+
+            // подписи для характеристик
+            string characteristicType = db.characteristic_type.Single(c => c.id == characteristicId).name;
+            characteristicNames.Add(characteristicType);
+
+
+            var characteristicsList = new List<SelectListItem>();
+            for (int i = 0; i < characteristicNames.Count; i++)
+            {
+                characteristicsList.Add(new SelectListItem
+                {
+                    Value = i.ToString(),
+                    Text = characteristicNames[i],
+                    Selected = false
+                });
+            }
+
+            TempData["result"] = new Dictionary<string, object>
+                                     {
+                                         { "characteristics", characteristics }, 
+                                         { "chainNames", chainNames }, 
+                                         { "chainsProduct", chainsProduct }, 
+                                         { "chainsPosition", chainsPosition }, 
+                                         { "chainsPieceTypes", chainsPieceTypes }, 
+                                         { "characteristicNames", characteristicNames }, 
+                                         { "matterIds", matterIds },
+                                         { "characteristicsList", characteristicsList }
+                                     };
+
+            return RedirectToAction("Result");
+        }
+
+        /// <summary>
+        /// The result.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Thrown if there is no data.
+        /// </exception>
+        public ActionResult Result()
+        {
+            try
+            {
+                var result = TempData["result"] as Dictionary<string, object>;
+                if (result == null)
+                {
+                    throw new Exception("No data.");
+                }
+
+                foreach (var key in result.Keys)
+                {
+                    ViewData[key] = result[key];
+                }
+
+                TempData.Keep();
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("Error", e.Message);
+
+                ViewBag.Error = true;
+
+                ViewBag.ErrorMessage = e.Message;
+            }
+
+            return View();
+        }
+
+        /// <summary>
+        /// The extract chains.
+        /// </summary>
+        /// <param name="pieces">
+        /// The pieces.
+        /// </param>
+        /// <param name="chainId">
+        /// The chain id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        private List<Chain> ExtractChains(List<piece> pieces, long chainId)
+        {
+            var starts = pieces.Select(p => p.start).ToList();
+
+            var stops = pieces.Select(p => p.start + p.length).ToList();
+
+            BaseChain parentChain = chainRepository.ToLibiadaBaseChain(chainId);
+
+            var iterator = new DefaultCutRule(starts, stops);
+
+            var stringChains = DiffCutter.Cut(parentChain.ToString(), iterator);
+
+            var chains = new List<Chain>();
+
+            for (int i = 0; i < stringChains.Count; i++)
+            {
+                chains.Add(new Chain(stringChains[i]));
+            }
+
+            return chains;
+        }
+    }
+}
