426baa2b53a95ff500fa3e9b93e66557e9427b64
NickName670 <nick680@gmail.com>
2014-12-24 02:06:08 +0600

    Updated db_integrity_test function.

--- LibiadaWeb/Db/Changelog.sql
+++ LibiadaWeb/Db/Changelog.sql
@@ -21,4 +21,92 @@ CREATE INDEX ix_piece_gene_id ON piece (gene_id ASC NULLS LAST);
 
 INSET INTO piece_type (name, description, nature_id) VALUES ('Различная РНК', 'misc_RNA - miscellaneous other RNA', 1)
 
+-- 24.12.2014
+-- Updating db_integrity_test function.
+
+DROP FUNCTION db_integrity_test();
+
+CREATE OR REPLACE FUNCTION db_integrity_test()
+  RETURNS void AS
+$BODY$
+function CheckChain() {
+    plv8.elog(INFO, "Проверяем целостность таблицы chain и её потомков.");
+
+    var chain = plv8.execute('SELECT id FROM chain');
+    var chainDistinct = plv8.execute('SELECT DISTINCT id FROM chain');
+    if (chain.length != chainDistinct.length) {
+        plv8.elog(ERROR, 'id таблицы chain и/или дочерних таблиц не уникальны.');
+    }else{
+		plv8.elog(INFO, "id всех цепочек уникальны.");
+    }
+	
+    plv8.elog(INFO, "Проверяем соответствие всех записей таблицы chain и её наследников с записями в таблице chain_key.");
+    
+    var chainDisproportion = plv8.execute('SELECT c.id, ck.id FROM (SELECT id FROM chain UNION SELECT id FROM gene) c FULL OUTER JOIN chain_key ck ON ck.id = c.id WHERE c.id IS NULL OR ck.id IS NULL');
+    
+    if (chainDisproportion.length > 0) {
+		var debugQuery = 'SELECT c.id chain_id, ck.id chain_key_id FROM (SELECT id FROM chain UNION SELECT id FROM gene) c FULL OUTER JOIN chain_key ck ON ck.id = c.id WHERE c.id IS NULL OR ck.id IS NULL';
+        plv8.elog(ERROR, 'Количество записей в таблице chain_key не совпадает с количеством записей с таблице chain и её наследниках. Для подробностей выполните "', debugQuery, '".');
+    }else{
+		plv8.elog(INFO, "Все записи в таблицах цепочек однозначно соответствуют записям в таблице chain_key.");
+    }
+	
+	plv8.elog(INFO, 'Таблицы цепочек успешно проверены.');
+}
+
+function CheckElement() {
+    plv8.elog(INFO, "Проверяем целостность таблицы element и её потомков.");
+
+    var element = plv8.execute('SELECT id FROM element');
+    var elementDistinct = plv8.execute('SELECT DISTINCT id FROM element');
+    if (element.length != elementDistinct.length) {
+        plv8.elog(ERROR, 'id таблицы element и/или дочерних таблиц не уникальны.');
+    }else{
+		plv8.elog(INFO, "id всех элементов уникальны.");
+    }
+
+    plv8.elog(INFO, "Проверяем соответствие всех записей таблицы element и её наследников с записями в таблице element_key.");
+    
+    var elementDisproportion = plv8.execute('SELECT c.id, ck.id FROM element c FULL OUTER JOIN element_key ck ON ck.id = c.id WHERE c.id IS NULL OR ck.id IS NULL');
+    
+    if (elementDisproportion.length > 0) {
+		var debugQuery = 'SELECT c.id, ck.id FROM element c FULL OUTER JOIN element_key ck ON ck.id = c.id WHERE c.id IS NULL OR ck.id IS NULL';
+        plv8.elog(ERROR, 'Количество записей в таблице element_key не совпадает с количеством записей с таблице element и её наследниках. Для подробностей выполните "', debugQuery,'"');
+    }else{
+		plv8.elog(INFO, "Все записи в таблицах элементов однозначно соответствуют записям в таблице element_key.");
+    }
+	
+	plv8.elog(INFO, 'Таблицы элементов успешно проверены.');
+}
+
+function CheckAlphabet() {
+	plv8.elog(INFO, 'Проверяем алфавиты всех цепочек.');
+	
+	var orphanedElements = plv8.execute('SELECT c.a FROM (SELECT DISTINCT unnest(alphabet) a FROM chain) c LEFT OUTER JOIN element_key e ON e.id = c.a WHERE e.id IS NULL');
+	if (orphanedElements.length > 0) { 
+		var debugQuery = 'SELECT c.a FROM (SELECT DISTINCT unnest(alphabet) a FROM chain) c LEFT OUTER JOIN element_key e ON e.id = c.a WHERE e.id IS NULL';
+		plv8.elog(ERROR, 'В БД отсутствует ', orphanedElements,' элементов алфавита. Для подробностей выполните "', debugQuery,'".');
+	}
+	else {
+		plv8.elog(INFO, 'Все элементы всех алфавитов присутствуют в таблице element_key.');
+	}
+	
+	//TODO: Проверить что все бинарные и однородныее характеристики вычислены для элементов присутствующих в алфавите.
+	plv8.elog(INFO, 'Все алфавиты цепочек успешно проверены.');
+}
+
+function db_integrity_test() {
+    plv8.elog(INFO, "Процедура проверки целостности БД запущена.");
+    CheckChain();
+    CheckElement();
+    CheckAlphabet();
+    plv8.elog(INFO, "Проверка целостности успешно завершена.");
+}
+
+db_integrity_test();
+$BODY$
+  LANGUAGE plv8 VOLATILE;
+
+COMMENT ON FUNCTION db_integrity_test() IS 'Функция для проверки целостности данных в базе.';
+
 COMMIT;
