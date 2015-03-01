99e0c57e83807191a1dd6bcf9cdf43ae4dbadefb
Vladimir Tsymbal <VladimirTS1991@gmail.com>
2015-03-01 15:40:37 +0600

    Add Google Charts to local characteristics

--- LibiadaWeb/App_Start/BundleConfig.cs
+++ LibiadaWeb/App_Start/BundleConfig.cs
@@ -74,6 +74,9 @@
                         "~/Scripts/jQAllRangeSliders-withRuler-min.js",
                         "~/Scripts/jquery.mousewheel.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/googlechart").Include(
+                        "~/Scripts/jsapi.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css", 
                       "~/Content/site.css"));
