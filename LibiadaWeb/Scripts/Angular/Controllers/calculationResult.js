1956b17b592a4a689e7857d8c70200adee73c2e7
Nikolay Pozdnichenko <nick680@gmail.com>
2015-11-14 19:39:31 +0600

    New version of charts.

--- LibiadaWeb/Scripts/Angular/Controllers/calculationResult.js
+++ LibiadaWeb/Scripts/Angular/Controllers/calculationResult.js
@@ -20,17 +20,40 @@
         }
 
         function drawScatter(points) {
-            var margin = { top: 30, right: 20, bottom: 30, left: 200 };
+            var margin = { top: 30, right: 20, bottom: 60, left: 60 };
             var width = 800 - margin.left - margin.right;
             var height = 600 - margin.top - margin.bottom;
 
-            var x = d3.scale.linear().range([0, width]);
-            var y = d3.scale.linear().range([height, 0]);
+            // setup x 
+            var xValue = function (d) { return d.x; }, // data -> value
+                xScale = d3.scale.linear().range([0, width]); // value -> display
+            //    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+            //   xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+            // setup y
+
+            var yValue = function (d) { return d.y; }, // data -> value
+                yScale = d3.scale.linear().range([height, 0]); // value -> display
+            // yMap = function(d) { return yScale(yValue(d));}, // data -> display
+            // yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+
+
+            var xMin = d3.min(points, xValue);
+            var yMin = d3.min(points, yValue);
+            var xMax = d3.max(points, xValue);
+            var yMax = d3.max(points, yValue);
+
+            // don't want dots overlapping axis, so add in buffer to data domain
+            xScale.domain([xMin, xMax]);
+            yScale.domain([yMin, yMax]);
+
 
             var chart = dc.seriesChart("#chart");
             var ndx = crossfilter(points);
             var runDimension = ndx.dimension(function (d, index) {
-                 return [d.name, +d.x]; });
+                return [d.name, +d.x];
+            });
             var runGroup = runDimension.group().reduceSum(function (d) { return +d.y; });
 
             var symbolScale = d3.scale.ordinal().range(d3.svg.symbolTypes);
@@ -44,24 +67,29 @@
 
             chart
                 .chart(subChart)
-                .x(x)
-                .y(y)
+                .x(xScale)
+                .y(yScale)
                 .width(width)
                 .height(height)
-                .brushOn(false)
+                //.brushOn(false)
                 .xAxisLabel($scope.firstCharacteristic.Text)
                 .yAxisLabel($scope.secondCharacteristic.Text)
+                .xAxisPadding((xMax - xMin) * 0.05)
+                .yAxisPadding((yMax - yMin) * 0.05)
                 .clipPadding(10)
+                .margins(margin)
                 .elasticX(true)
                 .elasticY(true)
-                .dimension(runDimension)
+                //.dimension(runDimension)
                 .group(runGroup)
-                .mouseZoomable(true)
-                 .seriesAccessor(function (d) { return d.key[0]; })
-    .keyAccessor(function (d) { return d.key[1]; })
-    .valueAccessor(function (d) { return +d.value; })
-                .legend(dc.legend().x(350).y(350).itemHeight(13).gap(5).horizontal(1).legendWidth(140).itemWidth(70));
+                // .mouseZoomable(true)
+                .seriesAccessor(function (d) { return d.key[0]; })
+                .keyAccessor(function (d) { return d.key[1]; })
+                .valueAccessor(function (d) { return +d.value; });
+            //.legend(dc.legend().x(350).y(350).itemHeight(13).gap(5).horizontal(1).legendWidth(140).itemWidth(70));
             //chart.yAxis().tickFormat(function (d) { return d3.format(",d")(d + 299500); });
+
+
 
             dc.renderAll();
         }
