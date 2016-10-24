985ccd3e9020f0972991843a8fcfb9d1943a77b4
elijahblack <elijah.black.23@gmail.com>
2016-10-24 17:37:42 +0600

    Add Task Manager Web Api
    .

---
+++ LibiadaWeb/Controllers/TaskManagerWebApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using LibiadaWeb.Tasks;
+
+namespace LibiadaWeb.Controllers
+{
+    [Authorize]
+    public class TaskManagerWebApiController : ApiController
+    {
+        public string GetTaskData(int id)
+        {
+            var task = TaskManager.GetTask(id);
+
+            if (task.TaskData.TaskState == TaskState.Completed)
+            {
+
+                return task.Result["data"].ToString();
+
+            }
+
+            throw new Exception();
+        }
+    }
+}
