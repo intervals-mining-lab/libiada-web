7f788b1983c6aa69a7f7a7669d70bcaca27d62e9
Artemiy Skiba <skiba.artem@inbox.ru>
2016-11-30 23:32:42 +0600

    Subsequences comparer result view filter by product added

--- LibiadaWeb/Scripts/Angular/Controllers/subsequencesComparerResult.js
+++ LibiadaWeb/Scripts/Angular/Controllers/subsequencesComparerResult.js
@@ -11,11 +11,75 @@
         }).success(function (data) {
             MapModelFromJson($scope, JSON.parse(data));
 
+            for (var i = 0; i < $scope.equalElements.length; i++) {
+                $scope.equalElements[i].filtersVisible = [];
+            }
+
             $scope.loading = false;
         }).error(function (data) {
             alert("Failed loading characteristic data");
         });
-    }
 
+        // adds and applies new filter
+        function addFilter() {
+            if ($scope.newFilter.length > 0) {
+                $scope.filters.push({ value: $scope.newFilter });
+
+                for(var i = 0; i < $scope.equalElements.length; i++) {
+                    var firstProductId = $scope.getFirstProductAttributeId($scope.equalElements[i]);
+
+                    var firstVisible = firstProductId && $scope.attributeValues[firstProductId].value.toUpperCase().indexOf($scope.newFilter.toUpperCase()) !== -1;
+
+                    var secondProductId = $scope.getSecondProductAttributeId($scope.equalElements[i]);
+
+                    var secondVisible = secondProductId && $scope.attributeValues[secondProductId].value.toUpperCase().indexOf($scope.newFilter.toUpperCase()) !== -1;
+
+                    $scope.equalElements[i].filtersVisible.push(firstVisible || secondVisible);
+                }
+
+                $scope.newFilter = "";
+            }
+            // todo: add error message if filter is empty
+        }
+
+        // checks if element is visible
+        function elementVisible(element) {
+            return element.filtersVisible.length === 0 || element.filtersVisible.some(function (element) {
+                return element;
+            });
+        }
+
+        // deletes given filter
+        function deleteFilter(filter) {
+            for (var i = 0; i < $scope.equalElements.length; i++) {
+                $scope.equalElements[i].filtersVisible.splice($scope.filters.indexOf(filter), 1);
+            }
+
+            $scope.filters.splice($scope.filters.indexOf(filter), 1);
+        }
+
+        // returns first product attribute index if any
+        function getFirstProductAttributeId(equalElement) {
+            return $scope.characteristics[equalElement.FirstMatterId][equalElement.FirstSubsequenceId].Attributes.find(function (a) {
+                return $scope.attributes[$scope.attributeValues[a].attribute] === "product";
+            });
+        }
+
+        // returns second product attribute index if any
+        function getSecondProductAttributeId(equalElement) {
+            return $scope.characteristics[equalElement.SecondMatterId][equalElement.SecondSubsequenceId].Attributes.find(function (a) {
+                return $scope.attributes[$scope.attributeValues[a].attribute] === "product";
+            });
+        }
+
+        $scope.addFilter = addFilter;
+        $scope.deleteFilter = deleteFilter;
+        $scope.getFirstProductAttributeId = getFirstProductAttributeId;
+        $scope.getSecondProductAttributeId = getSecondProductAttributeId;
+        $scope.elementVisible = elementVisible;
+
+        $scope.filters = [];
+    }
+    
     angular.module("SubsequencesComparerResult", []).controller("SubsequencesComparerResultCtrl", ["$scope", "$http", subsequencesComparerResult]);
 }
