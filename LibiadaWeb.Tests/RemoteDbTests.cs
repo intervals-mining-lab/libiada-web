710e2ced0f76012aaabf5b8cf2f1a4a651697e88
Nikolay Pozdnichenko <nick680@gmail.com>
2017-01-14 02:34:47 +0600

    Add RemoteDb and Translator enums tests

---
+++ LibiadaWeb.Tests/RemoteDbTests.cs
@@ -0,0 +1,109 @@
+﻿namespace LibiadaWeb.Tests
+{
+    using System.Linq;
+
+    using LibiadaCore.Extensions;
+
+    using LibiadaWeb.Extensions;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// RemoteDb enum tests.
+    /// </summary>
+    [TestFixture(TestOf = typeof(RemoteDb))]
+    public class RemoteDbTests
+    {
+        /// <summary>
+        /// The remote dbs count.
+        /// </summary>
+        private const int RemoteDbsCount = 1;
+
+        /// <summary>
+        /// Tests count of remote dbs.
+        /// </summary>
+        [Test]
+        public void RemoteDbCountTest()
+        {
+            var actualCount = ArrayExtensions.ToArray<RemoteDb>().Length;
+            Assert.AreEqual(RemoteDbsCount, actualCount);
+        }
+
+        /// <summary>
+        /// Tests values of remote dbs.
+        /// </summary>
+        [Test]
+        public void RemoteDbValuesTest()
+        {
+            var remoteDbs = ArrayExtensions.ToArray<RemoteDb>();
+
+            for (int i = 1; i <= RemoteDbsCount; i++)
+            {
+                Assert.IsTrue(remoteDbs.Contains((RemoteDb)i));
+            }
+        }
+
+        /// <summary>
+        /// Tests names of remote dbs.
+        /// </summary>
+        /// <param name="remoteDb">
+        /// The remote db.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        [TestCase((RemoteDb)1, "GenBank")]
+        public void RemoteDbNamesTest(RemoteDb remoteDb, string name)
+        {
+            Assert.AreEqual(name, remoteDb.GetName());
+        }
+
+        /// <summary>
+        /// Tests that all remote dbs have display value.
+        /// </summary>
+        /// <param name="remoteDb">
+        /// The remote db.
+        /// </param>
+        [Test]
+        public void RemoteDbHasDisplayValueTest([Values]RemoteDb remoteDb)
+        {
+            Assert.IsFalse(string.IsNullOrEmpty(remoteDb.GetDisplayValue()));
+        }
+
+        /// <summary>
+        /// Tests that all remote dbs have description.
+        /// </summary>
+        /// <param name="remoteDb">
+        /// The remote db.
+        /// </param>
+        [Test]
+        public void RemoteDbHasDescriptionTest([Values]RemoteDb remoteDb)
+        {
+            Assert.IsFalse(string.IsNullOrEmpty(remoteDb.GetDescription()));
+        }
+
+        /// <summary>
+        /// Tests that all remote dbs have valid nature attribute.
+        /// </summary>
+        /// <param name="remoteDb">
+        /// The remote db.
+        /// </param>
+        [Test]
+        public void RemoteDbHasNatureTest([Values]RemoteDb remoteDb)
+        {
+            var natures = ArrayExtensions.ToArray<Nature>();
+            Assert.True(natures.Contains(remoteDb.GetNature()));
+        }
+
+        /// <summary>
+        /// Tests that all remote dbs values are unique.
+        /// </summary>
+        [Test]
+        public void RemoteDbValuesUniqueTest()
+        {
+            var remoteDbs = ArrayExtensions.ToArray<RemoteDb>();
+            var remoteDbValues = remoteDbs.Cast<byte>();
+            Assert.That(remoteDbValues, Is.Unique);
+        }
+    }
+}
---
+++ LibiadaWeb.Tests/TranslatorTests.cs
@@ -0,0 +1,96 @@
+﻿namespace LibiadaWeb.Tests
+{
+    using System.Linq;
+
+    using LibiadaCore.Extensions;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Translator enum tests.
+    /// </summary>
+    [TestFixture(TestOf = typeof(Translator))]
+    public class TranslatorTests
+    {
+        /// <summary>
+        /// The translators count.
+        /// </summary>
+        private const int TranslatorsCount = 4;
+
+        /// <summary>
+        /// Tests count of translators.
+        /// </summary>
+        [Test]
+        public void TranslatorCountTest()
+        {
+            var actualCount = ArrayExtensions.ToArray<Translator>().Length;
+            Assert.AreEqual(TranslatorsCount, actualCount);
+        }
+
+        /// <summary>
+        /// Tests values of translators.
+        /// </summary>
+        [Test]
+        public void TranslatorValuesTest()
+        {
+            var translators = ArrayExtensions.ToArray<Translator>();
+            for (int i = 0; i < TranslatorsCount; i++)
+            {
+                Assert.IsTrue(translators.Contains((Translator)i));
+            }
+        }
+
+        /// <summary>
+        /// Tests names of translators.
+        /// </summary>
+        /// <param name="translator">
+        /// The translator.
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        [TestCase((Translator)0, "NoneOrManual")]
+        [TestCase((Translator)1, "GoogleTranslate")]
+        [TestCase((Translator)2, "Promt")]
+        [TestCase((Translator)3, "InterTran")]
+        public void TranslatorNamesTest(Translator translator, string name)
+        {
+            Assert.AreEqual(name, translator.GetName());
+        }
+
+        /// <summary>
+        /// Tests that all translators have display value.
+        /// </summary>
+        /// <param name="translator">
+        /// The translator.
+        /// </param>
+        [Test]
+        public void TranslatorHasDisplayValueTest([Values]Translator translator)
+        {
+            Assert.IsFalse(string.IsNullOrEmpty(translator.GetDisplayValue()));
+        }
+
+        /// <summary>
+        /// Tests that all translators have description.
+        /// </summary>
+        /// <param name="translator">
+        /// The translator.
+        /// </param>
+        [Test]
+        public void TranslatorHasDescriptionTest([Values]Translator translator)
+        {
+            Assert.IsFalse(string.IsNullOrEmpty(translator.GetDescription()));
+        }
+
+        /// <summary>
+        /// Tests that all translators values are unique.
+        /// </summary>
+        [Test]
+        public void TranslatorValuesUniqueTest()
+        {
+            var translators = ArrayExtensions.ToArray<Translator>();
+            var translatorValues = translators.Cast<byte>();
+            Assert.That(translatorValues, Is.Unique);
+        }
+    }
+}
