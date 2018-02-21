40d731e4e70e928c19e12b155b00faf3b19cbb65
berendeev <sellect.47@gmail.com>
2018-02-21 15:12:35 +0600

    order generators fixes and refactorings

--- LibiadaWeb/Controllers/Calculators/SequencesOrderDistributionController.cs
+++ LibiadaWeb/Controllers/Calculators/SequencesOrderDistributionController.cs
@@ -1,66 +1,85 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using LibiadaCore.Core;
-using LibiadaWeb.Tasks;
-using Newtonsoft.Json;
-using SequenceGenerator;
-
-namespace LibiadaWeb.Controllers.Calculators
+﻿namespace LibiadaWeb.Controllers.Calculators
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using LibiadaCore.Core;
+
+    using LibiadaWeb.Tasks;
+
+    using Newtonsoft.Json;
+
+    using SequenceGenerator;
+
     /// <summary>
     /// Calculates distribution of sequences by order.
     /// </summary>
     [Authorize(Roles = "Admin")]
     public class SequencesOrderDistributionController : AbstractResultController
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SequencesOrderDistributionController"/> class.
+        /// </summary>
         public SequencesOrderDistributionController() : base(TaskType.SequencesOrderDistribution)
         {
         }
 
-        // GET: SequencesOrderDestribution
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <param name="length">
+        /// The length.
+        /// </param>
+        /// <param name="alphabetCardinality">
+        /// The alphabet cardinality.
+        /// </param>
+        /// <param name="generateStrict">
+        /// The generate strict.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
         [HttpPost]
         public ActionResult Index(int length, int alphabetCardinality, bool generateStrict)
         {
             return CreateTask(() =>
             {
-                var sequnceGenerator = new SequenceGenerator.SequenceGenerator();
+                var sequenceGenerator = generateStrict ?
+                                       (ISequenceGenerator)new StrictSequenceGenerator() :
+                                       new SequenceGenerator();
                 var orderGenerator = new OrderGenerator();
-                List<BaseChain> sequnces;
-                List<int[]> orders;
-                if (generateStrict)
-                {
-                    sequnces = sequnceGenerator.StrictGenerateSequences(length, alphabetCardinality);
-                    orders = orderGenerator.StrictGenerateOrders(length, alphabetCardinality);
-                }
-                else
-                {
-                    sequnces = sequnceGenerator.GenerateSequences(length, alphabetCardinality);
-                    orders = orderGenerator.GenerateOrders(length, alphabetCardinality);
-                }
+                var sequences = sequenceGenerator.GenerateSequences(length, alphabetCardinality);
+                var orders = orderGenerator.GenerateOrders(length, alphabetCardinality);
                 Dictionary<int[], List<BaseChain>> result = new Dictionary<int[], List<BaseChain>>(new OrderEqualityComparer());
                 foreach (var order in orders)
                 {
                     result.Add(order, new List<BaseChain>());
                 }
-                foreach (var sequence in sequnces)
+                foreach (var sequence in sequences)
                 {
                     result[sequence.Building].Add(sequence);
                 }
 
                 var data = new Dictionary<string, object>
                 {
-                    { "result",result.Select(r => new{order = r.Key, sequences = r.Value.Select(s => s.ToString(",")).ToArray()}) }
+                    { "result", result.Select(r => new { order = r.Key, sequences = r.Value.Select(s => s.ToString(",")).ToArray() }) }
                 };
 
                 return new Dictionary<string, object>
                 {
-                    {"data", JsonConvert.SerializeObject(data)}
+                    { "data", JsonConvert.SerializeObject(data) }
                 };
             });
         }
