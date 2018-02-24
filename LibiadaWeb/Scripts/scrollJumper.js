85c7122070eb1d45e6f5e790b6cce51b11ba4b3f
bog5651 <div98@mail.ru>
2018-02-24 21:43:10 +0600

    Add event on scroll

--- LibiadaWeb/Scripts/scrollJumper.js
+++ LibiadaWeb/Scripts/scrollJumper.js
@@ -30,5 +30,43 @@
         }
         
     }
-    );
+	);
+	//mouseover
+	var timeDelay = 10;
+	var alphabet = ["f", "e", "b", "c", "b", "a", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+	$('#buttonScrollUp').mouseover(
+		function (e) {
+			var color;
+			var i=0;
+			var DescriptorInterval = setInterval(function () {
+				color = "#" + "f" + alphabet[i] + "f" + alphabet[i] + "f" + alphabet[i];
+				document.getElementById('buttonScrollUp').style.backgroundColor = color;
+				i++;
+				if (i == 16) clearInterval(DescriptorInterval);
+			}, timeDelay);
+	}
+	);
+	$('#buttonScrollDown').mouseover(
+		function (e) {
+			var color;
+			var i = 0;
+			var DescriptorInterval = setInterval(function () {
+				color = "#" + "f" + alphabet[i] + "f" + alphabet[i] + "f" + alphabet[i];
+				document.getElementById('buttonScrollDown').style.backgroundColor = color;
+				i++;
+				if (i == 16) clearInterval(DescriptorInterval);
+			}, timeDelay);
+		}
+	);
+	//mouseout
+	$('#buttonScrollUp').mouseout(
+		function (e) {
+			document.getElementById('buttonScrollUp').style.backgroundColor = "#fff";
+		}
+	);
+	$('#buttonScrollDown').mouseout(
+		function (e) {
+			document.getElementById('buttonScrollDown').style.backgroundColor = "#fff";
+		}
+	);
 });
