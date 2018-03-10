a42f3e90bb96f774647223febfbc88b2dfcd25f6
bog5651 <div98@mail.ru>
2018-03-10 21:29:24 +0600

    Sending task's user for all tabs of the user Sending all tasks to all connected administrators

--- LibiadaWeb/Tasks/TasksManagerHub.cs
+++ LibiadaWeb/Tasks/TasksManagerHub.cs
@@ -50,11 +50,11 @@ namespace LibiadaWeb.Tasks
                 };
 
                 int userId = task.UserId;
-                List<string> usersId = new List<string>{
-                    userId.ToString()
-                };
-
-                hubContext.Clients.Users(usersId).TaskEvent(taskEvent.ToString(), result);
+                if (!AccountHelper.IsAdmin())
+                {
+                    hubContext.Clients.Group(userId.ToString()).TaskEvent(taskEvent.ToString(), result);
+                }
+                hubContext.Clients.Group("admins").TaskEvent(taskEvent.ToString(), result);
             }
         }
 
@@ -72,21 +72,47 @@ namespace LibiadaWeb.Tasks
             var tasks = TaskManager.GetTasksData()
                 .Where(t => t.UserId == userId || isAdmin)
                 .Select(task => new
-            {
-                task.Id,
-                TaskType = task.TaskType.GetName(),
-                DisplayName = task.TaskType.GetDisplayValue(),
-                Created = task.Created.ToString(OutputFormats.DateTimeFormat),
-                Started = task.Started?.ToString(OutputFormats.DateTimeFormat),
-                Completed = task.Completed?.ToString(OutputFormats.DateTimeFormat),
-                ExecutionTime = task.ExecutionTime?.ToString(OutputFormats.TimeFormat),
-                TaskState = task.TaskState.ToString(),
-                TaskStateName = task.TaskState.GetDisplayValue(),
-                task.UserId,
-                task.UserName
-            });
+                {
+                    task.Id,
+                    TaskType = task.TaskType.GetName(),
+                    DisplayName = task.TaskType.GetDisplayValue(),
+                    Created = task.Created.ToString(OutputFormats.DateTimeFormat),
+                    Started = task.Started?.ToString(OutputFormats.DateTimeFormat),
+                    Completed = task.Completed?.ToString(OutputFormats.DateTimeFormat),
+                    ExecutionTime = task.ExecutionTime?.ToString(OutputFormats.TimeFormat),
+                    TaskState = task.TaskState.ToString(),
+                    TaskStateName = task.TaskState.GetDisplayValue(),
+                    task.UserId,
+                    task.UserName
+                });
 
             return JsonConvert.SerializeObject(tasks.ToArray());
+        }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            if (AccountHelper.IsAdmin())
+            {
+                Groups.Add(Context.ConnectionId, "admins");
+            }
+            else
+            {
+                Groups.Add(Context.ConnectionId, Context.User.Identity.GetUserId<int>().ToString());
+            }
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            if (AccountHelper.IsAdmin())
+            {
+                Groups.Remove(Context.ConnectionId, "admins"); // сомневаюсь что это правильно
+            }
+            else
+            {
+                Groups.Remove(Context.ConnectionId, Context.User.Identity.GetUserId<int>().ToString());
+            }
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
