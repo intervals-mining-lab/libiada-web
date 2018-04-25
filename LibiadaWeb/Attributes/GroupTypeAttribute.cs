cb27813cde2f9a574f9434750daf2974be6494ba
Julia Melnichenko <melnichenko_js@mail.ru>
2018-04-25 11:48:31 +0600

    Add new GroupType enum and attribute

---
+++ LibiadaWeb/Attributes/GroupTypeAttribute.cs
@@ -0,0 +1,36 @@
+﻿namespace LibiadaWeb.Attributes
+{
+    using System;
+
+    /// <summary>
+    /// The group type attribute.
+    /// Used to specify group types hierarchy.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class GroupTypeAttribute : System.Attribute
+    {
+        /// <summary>
+        /// The group type value.
+        /// </summary>
+        public readonly GroupType Value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GroupTypeAttribute"/> class.
+        /// </summary>
+        /// <param name="value">
+        /// The group type.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown if value is not a valid <see cref="GroupType"/>.
+        /// </exception>
+        public GroupTypeAttribute(GroupType value)
+        {
+            if (!Enum.IsDefined(typeof(GroupType), value))
+            {
+                throw new ArgumentException("GroupType attribute value is not valid group type", nameof(value));
+            }
+
+            Value = value;
+        }
+    }
+}
---
+++ LibiadaWeb/GroupType.cs
@@ -0,0 +1,37 @@
+﻿namespace LibiadaWeb
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    using LibiadaWeb.Attributes;
+
+    public enum GroupType : short
+    {
+        [Display(Name = "Prose")]
+        [Description("")]
+        [Nature(Nature.Literature)]
+        Prose = 1,
+
+        [Display(Name = "Poems and poetry")]
+        [Description("")]
+        [Nature(Nature.Literature)]
+        Poem = 2,
+
+        [Display(Name = "Domain")]
+        [Description("Domain")]
+        [Nature(Nature.Genetic)]
+        Domain = 3,
+
+        [Display(Name = "Kingdom")]
+        [Description("Kingdom")]
+        [Nature(Nature.Genetic)]
+        [GroupType(Domain)]
+        Kingdom = 4,
+
+        [Display(Name = "Phylum")]
+        [Description("Phylum")]
+        [Nature(Nature.Genetic)]
+        [GroupType(Kingdom)]
+        Phylum = 5
+    }
+}
