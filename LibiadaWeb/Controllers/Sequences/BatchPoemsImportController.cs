ec76c70738421012c4c15159f1ea5398c40f1556
Anastasia Kvashnina <anastasia.kv97@gmail.com>
2018-11-10 14:43:10 +0600

    Batch poems import implementation

--- LibiadaWeb/Controllers/Sequences/BatchPoemsImportController.cs
+++ LibiadaWeb/Controllers/Sequences/BatchPoemsImportController.cs
@@ -1,7 +1,11 @@
 ﻿
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using LibiadaWeb.Extensions;
+using LibiadaWeb.Models.CalculatorsData;
+using LibiadaWeb.Models.Repositories.Sequences;
 using LibiadaWeb.Tasks;
 using Newtonsoft.Json;
 
@@ -28,7 +32,68 @@ namespace LibiadaWeb.Controllers.Sequences
         [ValidateAntiForgeryToken]
         public ActionResult Index(Notation notation)
         {
-            return View();
+            return CreateTask(() =>
+            {
+                var importResults = new List<MatterImportResult>();
+
+                using (var db = new LibiadaWebEntities())
+                {
+                    Matter[] matters = db.Matter.Where(m => m.Nature == Nature.Literature).ToArray();
+
+                    for (int i = 0; i < Request.Files.Count; i++)
+                    {
+                        var importResult = new MatterImportResult();
+
+                        string sequenceName = Request.Files[i].FileName;
+                        try
+                        {
+                            CommonSequence sequence = new CommonSequence
+                            {
+                                Notation = notation
+                            };
+
+
+                            if (matters.Any(m => m.Name == sequenceName))
+                            {
+                                sequence.MatterId = matters.Single(m => m.Name == sequenceName).Id;
+                                importResult.Result = "Successfully imported poem for existing matter";
+                            }
+                            else
+                            {
+                                sequence.Matter = new Matter
+                                {
+                                    Name = sequenceName,
+                                    Group = Group.ClassicalLiterature,
+                                    Nature = Nature.Literature,
+                                    SequenceType = SequenceType.CompleteText
+                                };
+
+                                importResult.Result = "Successfully imported poem and created matter";
+                            }
+
+                            LiteratureSequenceRepository repository = new LiteratureSequenceRepository(db);
+
+                            repository.Create(sequence, Request.Files[i].InputStream, Language.Russian, true,
+                                Translator.NoneOrManual);
+                            importResult.Status = "Success";
+                            importResults.Add(importResult);
+                        }
+                        catch (Exception exception)
+                        {
+                            importResult.Result = $"Failed to import poem: {exception.Message}";
+                            importResult.Status = "Error";
+                            importResults.Add(importResult);
+                        }
+                    }
+
+                    var data = new Dictionary<string, object> { { "result", importResults } };
+
+                    return new Dictionary<string, object>
+                    {
+                        {"data", JsonConvert.SerializeObject(data)}
+                    };
+                }
+            });
         }
     }
 }
