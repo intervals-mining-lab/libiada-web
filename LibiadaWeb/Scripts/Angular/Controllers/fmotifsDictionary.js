05dd1ef66c1c095781a2b71850e7b1fcb9217307
Gertsog <asgertsog@gmail.com>
2019-05-25 13:48:39 +0600

    Fix MIDI delay

--- LibiadaWeb/Scripts/Angular/Controllers/fmotifsDictionary.js
+++ LibiadaWeb/Scripts/Angular/Controllers/fmotifsDictionary.js
@@ -17,13 +17,13 @@
                     //MIDI.programChange(0, MIDI.GM.byName["acoustic_guitar_steel"].program);
                 }
             });
+
         };
 
         var player = {
             barDuration: 4,
             timeline: 0,
             velocity: 127,
-            tempAlts: {},
             playNote: function (pitch, duration, moveTime) {
                 MIDI.noteOn(0, pitch, this.velocity, this.timeline);
                 MIDI.noteOff(0, pitch, this.velocity, this.timeline + this.barDuration * duration);
@@ -42,34 +42,31 @@
             },
             move: function (duration) {
                 this.timeline += this.barDuration * duration;
-                if (this.isEndOfBar()) {
-                    this.tempAlts = {};
-                }
-            },
-            isEndOfBar: function () {
-                return !!(this.timeline % this.barDuration === 0);
             }
         };
 
         $scope.play = function (event) {
             MIDI.setVolume(0, 80);
             var notes = $scope.data.fmotifs[event].NoteList;
+            var octaver = $scope.data.sequentialTransfer ? 60 : 0;
             for (var i = 0; i < notes.length; i++) {
                 if (notes[i].Pitches.length > 1) {
                     var pitches = [];
                     for (var j = 0; j < notes[i].Pitches.length; j++) {
-                        pitches[j] = notes[i].Pitches[j].MidiNumber;
+                        pitches[j] = notes[i].Pitches[j].MidiNumber + octaver;
                     }
                     player.playChord(pitches, notes[i].Duration.Value, true);
                 }
                 else if (notes[i].Pitches.length === 1) {
-                    player.playNote(notes[i].Pitches[0].MidiNumber, notes[i].Duration.Value, true);
+                    player.playNote(notes[i].Pitches[0].MidiNumber + octaver, notes[i].Duration.Value, true);
                 }
                 else {
                     player.playNote(0, notes[i].Duration.Value, true);
                 }
-            }                      
+            }
+            player.timeline = 0;
         };
+
     }
 
     angular.module("libiada").controller("FmotifsDictionaryCtrl", ["$scope", fmotifsDictionary]);
