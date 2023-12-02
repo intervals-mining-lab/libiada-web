95447e7ce4b70f72fb9d46e1001b1a2abbdc99b7
Nikolay Pozdnichenko <nick680@gmail.com>
2023-12-03 00:47:31 +0600

    Fix data visualization page (charts)

--- LibiadaWeb/Scripts/Angular/Controllers/charts.js
+++ LibiadaWeb/Scripts/Angular/Controllers/charts.js
@@ -1,119 +1,343 @@
-﻿angular.module("libiada", ["Directives.TableParse"])
-    .controller("ChartsCtrl", ["$scope", function ($scope) {
+﻿function ChartsController(data) {
+    "use strict";
+
+    function charts($scope, $document) {
+        function parseTabularData(text) {
+            //The array we will return
+            let result = [];
+            try {
+                //Pasted data split into rows
+                let rows = text.split(/[\n\f\r]/);
+                rows.forEach(thisRow => {
+                    let row = thisRow.trim();
+                    if (row != "") {
+                        let cols = row.split("\t").map(c => c.replace(",", "."));
+                        result.push(cols);
+                    }
+                });
+            }
+            catch (err) {
+                console.log("error parsing as tabular data");
+                console.log(err);
+                return null;
+            }
+            return result;
+        }
+
+        function textChanged() {
+            let text = $("#dataPasteBox").val();
+            if (text != "") {
+                $scope.rawData = text;
+                let asArray = $scope.parseTabularData(text);
+                if (asArray != null) {
+                    $scope.parsedData = asArray;
+                    $scope.$apply();
+                }
+            }
+        }
+
+        function handleKeyDown(e, args) {
+            if (!$scope.inFocus && e.which == $scope.keyCodes.V && (e.ctrlKey || e.metaKey)) { // CTRL + V
+                //reset value of our box
+                $("#dataPasteBox").val("");
+                //set it in focus so that pasted text goes inside the box
+                $("#dataPasteBox").focus();
+            }
+        }
+
+        $document.ready(() => {
+            //Handles the Ctrl + V keys for pasting
+            
+
+            //If this is true, we wont respond to Ctrl + V
+            $("body").on("focus", "input, textarea", () => { $scope.inFocus = true; });
+
+            //We are not on a text element so we will respond
+            //to Ctrl + V
+            $("body").on("blur", "input, textarea", () => { $scope.inFocus = false; });
+
+            //Handle the key down event
+            $(document).keydown($scope.handleKeyDown);
+
+            //We will respond to when the textbox value changes
+            $("#dataPasteBox").bind("input propertychange", $scope.textChanged);
+        });
+
+        function fillLegend() {
+            $scope.legend = [];
+            for (let k = 1; k < $scope.parsedData.length; k++) {
+                $scope.legend.push({ id: k - 1, name: $scope.parsedData[k][0], visible: true });
+            }
+            $scope.legendHeight = $scope.legend.length * 20;
+        }
+
+        // initializes data for chart
+        function fillPoints() {
+
+            $scope.points = [];
+
+            for (let i = 1; i < $scope.parsedData.length; i++) {
+                let characteristic = $scope.parsedData[i];
+                $scope.points.push({
+                    id: i,
+                    name: characteristic[0],
+                    x: +characteristic[$scope.firstCharacteristic],
+                    y: +characteristic[$scope.secondCharacteristic]
+                });
+            }
+        }
+
+
+        // constructs string representing tooltip text (inner html)
+        function fillPointTooltip(d) {
+            let tooltipContent = [];
+            tooltipContent.push("Name: " + d.name);
+
+
+            let pointsCharacteristics = [];
+            for (let i = 1; i < $scope.parsedData[0].length; i++) {
+                pointsCharacteristics.push($scope.parsedData[0][i] + ": " + $scope.parsedData[d.id][i]);
+            }
+
+            tooltipContent.push(pointsCharacteristics.join("<br/>"));
+
+            return tooltipContent.join("</br>");
+        }
+
+        // shows tooltip for dot or group of dots
+        function showTooltip(event, d, tooltip, svg) {
+            $scope.clearTooltip(tooltip);
+
+            tooltip.style("opacity", 0.9);
+
+            let tooltipHtml = [];
+
+            tooltip.selectedDots = svg.selectAll(".dot")
+                .filter((dot) => {
+                    if (dot.x === d.x && dot.y === d.y) {
+                        tooltipHtml.push($scope.fillPointTooltip(dot));
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+                .attr("rx", $scope.selectedDotRadius)
+                .attr("ry", $scope.selectedDotRadius);
+
+            tooltip.html(tooltipHtml.join("</br></br>"));
+
+            tooltip.style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 8) + "px");
+
+            tooltip.hideTooltip = false;
+        }
+
+        // clears tooltip and unselects dots
+        function clearTooltip(tooltip) {
+            if (tooltip) {
+                if (tooltip.hideTooltip) {
+                    tooltip.html("").style("opacity", 0);
+
+                    if (tooltip.selectedDots) {
+                        tooltip.selectedDots
+                            .attr("rx", $scope.dotRadius)
+                            .attr("ry", $scope.dotRadius);
+                    }
+                }
+
+                tooltip.hideTooltip = true;
+            }
+        }
+
+        function xValue(d) {
+            return d.x;
+        }
+
+        function yValue(d) {
+            return d.y;
+        }
 
         function draw() {
-            let margin = { top: 30, right: 20, bottom: 30, left: 200 },
-                width = 800 - margin.left - margin.right,
-                height = 600 - margin.top - margin.bottom;
+            $scope.fillLegend();
+            $scope.fillPoints();
 
-            // Set the ranges
-            let x = d3.scaleLinear().range([0, width]);
-            let y = d3.scaleLinear().range([height, 0]);
+            // removing previous chart and tooltip if any
+            d3.select(".chart-tooltip").remove();
+            d3.select(".chart-svg").remove();
 
-            // Define the axes
-            let xAxis = d3.svg.axis().scale(x)
-                .orient("bottom").ticks(5);
+            let actualLegendHeight = $scope.legendSettings.show ? $scope.legendHeight : 0;
 
-            let yAxis = d3.svg.axis().scale(y)
-                .orient("left").ticks(5);
+            // chart size and margin settings
+            let margin = { top: 30 + actualLegendHeight, right: 30, bottom: 35, left: 50 };
+            let width = $scope.width - margin.left - margin.right;
+            let height = $scope.height + actualLegendHeight - margin.top - margin.bottom;
 
-            // Define the line
-            let valueline = d3.svg.line()
-                .x(d => x(d.date))
-                .y(d => y(d.close));
+            // setup x
+            // calculating margins for dots
+            let xMin = d3.min($scope.points, $scope.xValue);
+            let xMax = d3.max($scope.points, $scope.xValue);
+            let xMargin = (xMax - xMin) * 0.05;
 
-            // Define 'div' for tooltips
-            let div = d3.select("body")
-                .append("div")  // declare the tooltip div
-                .attr("class", "chart-tooltip position-absolute text-bg-light font-monospace small lh-sm p-1 rounded")
-                .style("opacity", 0);                  // set the opacity to nil
+            let xScale = d3.scaleLinear()
+                .domain([xMin - xMargin, xMax + xMargin])
+                .range([0, width]);
+            let xAxis = d3.axisBottom(xScale)
+                .tickSizeInner(-height)
+                .tickSizeOuter(0)
+                .tickPadding(10);
 
-            // Adds the svg canvas
-            let svg = d3.select("body")
-                .append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
+            $scope.xMap = d => xScale($scope.xValue(d));
+
+            // setup y
+            // calculating margins for dots
+            let yMax = d3.max($scope.points, $scope.yValue);
+            let yMin = d3.min($scope.points, $scope.yValue);
+            let yMargin = (yMax - yMin) * 0.05;
+
+            let yScale = d3.scaleLinear()
+                .domain([yMin - yMargin, yMax + yMargin])
+                .range([height, 0]);
+            let yAxis = d3.axisLeft(yScale)
+                .tickSizeInner(-width)
+                .tickSizeOuter(0)
+                .tickPadding(10);
+
+            $scope.yMap = function (d) { return yScale($scope.yValue(d)); };
+
+            // setup fill color
+            let color = d3.scaleSequential(d3.interpolateTurbo).domain([0, $scope.legend.length]);
+
+            // add the graph canvas to the body of the webpage
+            let svg = d3.select("#chart").append("svg")
+                .attr("width", $scope.width)
+                .attr("height", $scope.height + actualLegendHeight)
+                .attr("class", "chart-svg")
                 .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            // Get the data
-            let data = $scope.data;
+            // add the tooltip area to the webpage
+            let tooltip = d3.select("#chart").append("div")
+                .attr("class", "chart-tooltip position-absolute text-bg-light font-monospace small lh-sm p-1 rounded")
+                .style("opacity", 0);
 
-            data.forEach(function (d) {
-                d.date = +d.date;
-                d.close = +d.close;
-                //d.name = +d.name;
-            });
+            // preventing tooltip hiding if dot clicked
+            tooltip.on("click", function () { tooltip.hideTooltip = false; });
 
-            // Scale the range of the data
-            x.domain(d3.extent(data, d => d.date));
-            y.domain(d3.extent(data, d => d.close));
+            // hiding tooltip
+            d3.select("#chart").on("click", function () { $scope.clearTooltip(tooltip); });
 
-            // Add the valueline path.
-            svg.append("path")
-                .attr("class", "line")
-                .attr("d", valueline(data));
-
-            // draw the scatterplot
-            svg.selectAll("dot")
-                .data(data)
-                .enter().append("circle")
-                .attr("r", 2)
-                .attr("cx", d => x(d.date))
-                .attr("cy", d => y(d.close))
-                // Tooltip stuff after this
-                .on("mouseover", (event, d) => {
-                    div.transition()
-                        .duration(700)
-                        .style("opacity", 0);
-                    div.transition()
-                        .duration(400)
-                        .style("opacity", .9);
-                    div.html(
-                        "Name = " + (d.name) +
-                        "<br/>x = " + (d.date) +
-                        "<br/>y = " + d.close)
-                        .style("left", (event.pageX) + "px")
-                        .style("top", (event.pageY - 28) + "px");
-                });
-
-            // Add the X Axis
+            // x-axis
             svg.append("g")
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + height + ")")
                 .call(xAxis);
 
-            // Add the Y Axis
+            svg.append("text")
+                .attr("class", "label")
+                .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top - actualLegendHeight) + ")")
+                .style("text-anchor", "middle")
+                .text($scope.firstCharacteristic.Text)
+                .style("font-size", "12pt");
+
+            // y-axis
             svg.append("g")
                 .attr("class", "y axis")
                 .call(yAxis);
-        };
 
+            svg.append("text")
+                .attr("class", "label")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 0 - margin.left)
+                .attr("x", 0 - (height / 2))
+                .attr("dy", ".71em")
+                .style("text-anchor", "middle")
+                .text($scope.secondCharacteristic.Text)
+                .style("font-size", "12pt");
+
+            // draw dots
+            svg.selectAll(".dot")
+                .data($scope.points)
+                .enter()
+                .append("ellipse")
+                .attr("class", "dot")
+                .attr("rx", $scope.dotRadius)
+                .attr("ry", $scope.dotRadius)
+                .attr("cx", $scope.xMap)
+                .attr("cy", $scope.yMap)
+                .style("fill-opacity", 0.6)
+                .style("fill", d => color(d.id))
+                .style("stroke", d => color(d.id))
+                .on("click", (event, d) => $scope.showTooltip(event, d, tooltip, svg));
+
+            if ($scope.legendSettings.show) {
+                // draw legend
+                let legend = svg.selectAll(".legend")
+                    .data($scope.legend)
+                    .enter()
+                    .append("g")
+                    .attr("class", "legend")
+                    .attr("transform", (_d, i) => "translate(0," + i * 20 + ")")
+                    .on("click", (event, d) => {
+                        d.visible = !d.visible;
+                        let legendEntry = d3.select(event.currentTarget);
+                        legendEntry.select("text")
+                            .style("opacity", () => d.visible ? 1 : 0.5);
+                        legendEntry.select("rect")
+                            .style("fill-opacity", () => d.visible ? 1 : 0);
+
+                        svg.selectAll(".dot")
+                            .filter(dot => dot.cluster === d.name)
+                            .attr("visibility", () => d.visible ? "visible" : "hidden");
+                    });
+
+                // draw legend colored rectangles
+                legend.append("rect")
+                    .attr("width", 15)
+                    .attr("height", 15)
+                    .style("fill", d => color(d.id))
+                    .style("stroke", d => color(d.id))
+                    .style("stroke-width", 4)
+                    .attr("transform", "translate(0, -" + $scope.legendHeight + ")");
+
+                // draw legend text
+                legend.append("text")
+                    .attr("x", 24)
+                    .attr("y", 9)
+                    .attr("dy", ".35em")
+                    .attr("transform", "translate(0, -" + $scope.legendHeight + ")")
+                    .text(d => ($scope.clustersCount ? "Cluster " : "") + d.name)
+                    .style("font-size", "9pt");
+            }
+        }
+
+        $scope.parseTabularData = parseTabularData;
+        $scope.textChanged = textChanged;
+        $scope.handleKeyDown = handleKeyDown;
         $scope.draw = draw;
+        $scope.fillPoints = fillPoints;
+        $scope.fillPointTooltip = fillPointTooltip;
+        $scope.showTooltip = showTooltip;
+        $scope.clearTooltip = clearTooltip;
+        $scope.fillLegend = fillLegend;
+        $scope.yValue = yValue;
+        $scope.xValue = xValue;
 
-        $scope.rawPaste = "";
-        $scope.parsedPaste = [];
-        $scope.data = [];
-        $scope.i = "";
-        $scope.j = "";
-        $scope.l = "";
-        $scope.data.date = [];
-        $scope.data.close = [];
-        $scope.data.name = [];
+        $scope.width = 800;
+        $scope.height = 800;
+        $scope.dotRadius = 3;
+        $scope.selectedDotRadius = $scope.dotRadius * 2;
+        $scope.legendSettings = { show: true };
+        $scope.characteristicsTableRendering = { rendered: false };
 
-        $scope.addX = (i, j) => {
-            $scope.data = [];
-            for (let k = 1; k < $scope.parsedPaste.length; k++) {
-                $scope.data.push({ date: $scope.parsedPaste[k][i].replace(",", "."), close: $scope.parsedPaste[k][j].replace(",", "."), name: $scope.parsedPaste[k][0] });
-            }
-            draw();
+        $scope.keyCodes = {
+            'C': 67,
+            'V': 86
         };
-        $scope.addY = (l) => {
-            $scope.data = [];
 
-            for (let k = 1; k < $scope.parsedPaste.length; k++) {
-                $scope.data.push({ date: k, close: $scope.parsedPaste[k][l].replace(",", "."), name: $scope.parsedPaste[k][0] });
-            }
-            draw();
-        };
-    }]);
+        $scope.inFocus = false;
+        $scope.rawData = "";
+        $scope.parsedData = [];
+    }
+
+    angular.module("libiada").controller("ChartsCtrl", ["$scope", "$document", charts]);
+}
