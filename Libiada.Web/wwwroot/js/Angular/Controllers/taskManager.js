70358d66917461ed27d0a73bc8cd6a6431c58672
Nikolay Pozdnichenko <nick680@gmail.com>
2024-02-24 18:43:54 +0600

    Fix task manager

--- Libiada.Web/wwwroot/js/Angular/Controllers/taskManager.js
+++ Libiada.Web/wwwroot/js/Angular/Controllers/taskManager.js
@@ -6,41 +6,38 @@
             alertify.error('Connection lost', 5);
         };
 
-        function onHubStart() {
-            $scope.$apply();
-            $scope.tasksHub.invoke("getAllTasks").then(tasks => {
-                for (let i = 0; i < tasks.length; i++) {
-                    let task = tasks[i];
-                    task.resultLink = `${window.location.origin}/${task.TaskType}/Result/${task.Id}`;
-                    $scope.tasks.push(task);
-                    $scope.tryRedirectToResult(task);
-                }
+        function onHubStart(tasks) {
+            for (let i = 0; i < tasks.length; i++) {
+                let task = tasks[i];
+                task.resultLink = `${window.location.origin}/${task.TaskType}/Result/${task.Id}`;
+                $scope.tasks.push(task);
+                $scope.tryRedirectToResult(task);
+            }
 
-                $scope.loading = false;
-                try {
-                    $scope.$apply();
-                } catch (e) { console.error(e.toString()); }
-            });
+            $scope.loading = false;
+            try {
+                $scope.$apply();
+            } catch (e) { console.error(e.toString()); }
         };
 
-        function taskEvent(event, data) {
+        function taskEvent(event, taskData) {
             switch (event) {
                 case "AddTask":
-                    $scope.tasks.push(data);
+                    $scope.tasks.push(taskData);
                     break;
                 case "DeleteTask":
-                    let taskToDelete = $scope.tasks.find(t => t.Id === data.Id);
+                    let taskToDelete = $scope.tasks.find(t => t.Id === taskData.Id);
                     $scope.tasks.splice($scope.tasks.indexOf(taskToDelete), 1);
                     break;
                 case "ChangeStatus":
-                    let taskToChange = $scope.tasks.find(t => t.Id === data.Id);
+                    let taskToChange = $scope.tasks.find(t => t.Id === taskData.Id);
                     if (taskToChange) {
-                        taskToChange.Created = data.Created;
-                        taskToChange.Started = data.Started;
-                        taskToChange.Completed = data.Completed;
-                        taskToChange.ExecutionTime = data.ExecutionTime;
-                        taskToChange.TaskState = data.TaskState;
-                        taskToChange.TaskStateName = data.TaskStateName;
+                        taskToChange.Created = taskData.Created;
+                        taskToChange.Started = taskData.Started;
+                        taskToChange.Completed = taskData.Completed;
+                        taskToChange.ExecutionTime = taskData.ExecutionTime;
+                        taskToChange.TaskState = taskData.TaskState;
+                        taskToChange.TaskStateName = taskData.TaskStateName;
 
                         $scope.tryRedirectToResult(taskToChange);
                     }
@@ -48,7 +45,7 @@
                         $scope.tasks.push(data);
                     }
                     break;
-                default: console.log("Unknown task event");
+                default: console.error("Unknown task event");
                     break;
             }
             try {
@@ -83,24 +80,24 @@
                 }, () => { });
         }
 
-        function deleteTasksWithStatus(taskState) {
-            alertify.confirm('Confirm action', `Are you sure you want to delete all tasks with "${taskState}" status?`,
+        function deleteTasksWithStatus(taskStatus) {
+            alertify.confirm('Confirm action', `Are you sure you want to delete all tasks with "${taskStatus}" status?`,
                 () => {
-                    $scope.tasks.filter(t => t.taskState == taskState).forEach(t => t.Deleting = true);
+                    $scope.tasks.filter(t => t.taskState === taskStatus).forEach(t => t.Deleting = true);
                     $scope.$apply();
-                    $scope.tasksHub.invoke("deleteTasksWithState", taskState)
-                        .then(() => alertify.success(`All tasks with "${taskState}" status have been deleted.`));
+                    $scope.tasksHub.invoke("deleteTasksWithState", taskStatus)
+                        .then(() => alertify.success(`All tasks with "${taskStatus}" status have been deleted.`));
                 }, () => { });
         }
 
         function deleteTask(id) {
             alertify.confirm('Confirm action', 'Are you sure you want to delete this task?',
                 () => {
-                    let taskToDelete = $scope.tasks.find(t => t.Id = id);
+                    let taskToDelete = $scope.tasks.find(t => t.Id === id);
                     taskToDelete.Deleting = true;
                     $scope.$apply();
                     $scope.tasksHub.invoke("deleteTask", id)
-                        .then(() => alertify.success('The task has been deleted.'));
+                        .then(() => alertify.success('The task has been deleted.'))
                 }, () => { });
         }
 
@@ -130,12 +127,12 @@
         $scope.tasksHub.on("taskEvent", $scope.taskEvent);
         $scope.tasksHub.onclose($scope.onCloseConnection);
 
-        $scope.tasksHub.start().then($scope.onHubStart).catch(function (err) {
-            return console.error(err.toString());
-        });
+        $scope.tasksHub.start()
+            .then(() => $scope.tasksHub.invoke("getAllTasks").then($scope.onHubStart))
+            .catch((err) => console.error(err.toString()));
 
         let location = window.location.href.split("/");
-        if (location[location.length - 1] != "TaskManager")
+        if (location[location.length - 1] !== "TaskManager")
             $scope.RedirectTaskId = location[location.length - 1];
         else $scope.RedirectTaskId = null;
         $scope.loadingScreenHeader = "Loading tasks";
