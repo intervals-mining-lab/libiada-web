cdfc4c6a89464328993bfdcac029ce5219ca5667
Andrey Karetnikov <andrew.karetnikov@gmail.com>
2025-04-20 22:27:49 +0600

    Added tsconfig.json, subsequencesDistribution.ts, modified property of tsconfig.json in  Libiada.Web.csproj

---
+++ Libiada.Web/wwwroot/js/Angular/Controllers/subsequencesDistribution.ts
@@ -0,0 +1,14 @@
+﻿declare var angular: any;
+
+// Объявляем существующую JavaScript функцию
+declare function MapModelFromJson($scope: any, data: any): void;
+
+function SubsequencesDistributionController(data) {
+    "use strict";
+
+    function subsequencesDistribution($scope) {
+        MapModelFromJson($scope, data);
+    }
+
+    angular.module("libiada").controller("SubsequencesDistributionCtrl", ["$scope", subsequencesDistribution]);
+}
