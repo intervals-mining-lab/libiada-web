6878ec8dbd4a1aff2072597978a2e3fc998dc32b
Andrey Karetnikov <andrew.karetnikov@gmail.com>
2025-04-20 22:36:36 +0600

    Refactor subsequencesDistribution.ts: added types

--- Libiada.Web/wwwroot/js/Angular/Controllers/subsequencesDistribution.ts
+++ Libiada.Web/wwwroot/js/Angular/Controllers/subsequencesDistribution.ts
@@ -1,14 +1,32 @@
-﻿declare var angular: any;
+﻿// Объявление типов
+interface IScope {
+    [key: string]: any;
+}
 
-// Объявляем существующую JavaScript функцию
+// Объявление глобальных переменных и функций
+declare const angular: any;
 declare function MapModelFromJson($scope: any, data: any): void;
 
-function SubsequencesDistributionController(data) {
-    "use strict";
-
-    function subsequencesDistribution($scope) {
-        MapModelFromJson($scope, data);
+// Основной класс контроллера
+class SubsequencesDistributionController {
+    constructor(private readonly data: any) {
+        this.initialize();
     }
 
-    angular.module("libiada").controller("SubsequencesDistributionCtrl", ["$scope", subsequencesDistribution]);
+    private initialize(): void {
+        // Определение функции контроллера
+        const subsequencesDistribution = ($scope: IScope): void => {
+            MapModelFromJson($scope, this.data);
+        };
+
+        // Регистрация контроллера в Angular модуле
+        angular.module("libiada")
+               .controller("SubsequencesDistributionCtrl", ["$scope", subsequencesDistribution]);
+    }
 }
+
+// Экспорт конструктора для использования в _AngularControllerInitializer.cshtml
+(window as any).SubsequencesDistributionController = function(data: any): void {
+    "use strict";
+    new SubsequencesDistributionController(data);
+};
