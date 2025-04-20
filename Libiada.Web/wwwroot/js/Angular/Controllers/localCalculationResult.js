b57f411c53ffbc1d7e7f22d57b405e8c42c559b8
RomanDev <mts.phone.56566@gmail.com>
2025-04-20 20:06:52 +0600

    Fix local calculation results visualizations

--- Libiada.Web/wwwroot/js/Angular/Controllers/localCalculationResult.js
+++ Libiada.Web/wwwroot/js/Angular/Controllers/localCalculationResult.js
@@ -60,16 +60,19 @@
 
             for (let i = 0; i < $scope.characteristics.length; i++) {
                 let characteristic = $scope.characteristics[i];
+                $scope.points.push({
+                    legendIndex: i,
+                    legendId: i + 1,
+                    researchObjectName: characteristic.ResearchObjectName,
+                    fragmentsData: []
+                });
                 for (let j = 0; j < characteristic.FragmentsData.length; j++) {
                     let fragmentData = characteristic.FragmentsData[j];
-                    $scope.points.push({
+                    $scope.points[i].fragmentsData.push({
                         id: j,
-                        characteristicId: i,
-                        legendIndex: i,
-                        legendId: i + 1,
+                        //characteristicId: i,
                         name: fragmentData.Name,
-                        characteristics: fragmentData.Characteristics,
-                        researchObjectName: characteristic.ResearchObjectName
+                        characteristics: fragmentData.Characteristics   
                     });
                 }
             }
@@ -123,13 +126,13 @@
         }
 
 
-        function fillBarPlotData() {
+        function fillLinePlotData() {
             let characteristicIndex = $scope.characteristicsList.indexOf($scope.chartCharacteristics[0].value);
-            let min = Math.min(...$scope.points.map(p => p.characteristics[characteristicIndex]));
-            let max = Math.max(...$scope.points.map(p => p.characteristics[characteristicIndex]));
+            let characteristicsValues = $scope.points.map((p => p.fragmentsData.map(fd => fd.characteristics[characteristicIndex]))).flat();
+            let min = Math.min(...characteristicsValues);
+            let max = Math.max(...characteristicsValues);
             let range = Math.abs(max - min);
-            let maxNameLength = Math.max(...$scope.points.map(p => p.name.length));
-
+           
             // adding margins
             min -= Math.abs(range * 0.05);
             max += Math.abs(range * 0.05);
@@ -139,7 +142,7 @@
                     l: 50,
                     r: 20,
                     t: 10,
-                    b: Math.min(150, maxNameLength * 10)
+                    b: 20
 
                 },
                 showlegend: false,
@@ -153,17 +156,21 @@
 
             $scope.chartData = $scope.points.map(p => ({
                 hoverinfo: "text+x+y",
-                x: [p.id],
-                y: [p.characteristics[characteristicIndex]],
-                marker: { color: $scope.legend[p.legendIndex].color },
+                x: p.fragmentsData.map(fd => fd.id),
+                y: p.fragmentsData.map(fd => fd.characteristics[characteristicIndex]),
+                marker: {
+                    color: $scope.legend[p.legendIndex].color,
+                    size: 2,
+                    opacity: 0.8
+                },
                 type: "scatter",
                 line: {
                     color: $scope.legend[p.legendIndex].color,
-                    width: 3
+                    width: 0.75
                 },
                 mode: "lines+markers",
                 customdata: { legendId: p.legendId },
-                name: p.name,
+                name: p.researchObjectName,
                 visible: $scope.legend[p.legendIndex].visible ? "true" : "legendonly"
             }));
         }
@@ -198,12 +205,16 @@
             $scope.chartData = $scope.points.map(p => ({
                 hoverinfo: "text+x+y",
                 type: "scattergl",
-                x: [p.characteristics[firstCharacteristicIndex]],
-                y: [p.characteristics[secondCharacteristicIndex]],
-                text: p.name,
+                x: p.fragmentsData.map(fd => fd.characteristics[firstCharacteristicIndex]),     
+                y: p.fragmentsData.map(fd => fd.characteristics[secondCharacteristicIndex]),
+                text: p.researchObjectName,
                 mode: "markers",
-                marker: { opacity: 0.8, color: $scope.legend[p.legendIndex].color },
-                name: p.name,
+                marker: {
+                    opacity: 0.8,
+                    color: $scope.legend[p.legendIndex].color,
+                    size: 3
+                },
+                name: p.researchObjectName,
                 customdata: { legendId: p.legendId },
                 visible: $scope.legend[p.legendIndex].visible
             }));
@@ -217,13 +228,17 @@
 
             $scope.chartData = $scope.points.map(p => ({
                 hoverinfo: "text+x+y+z",
-                x: [p.characteristics[firstCharacteristicIndex]],
-                y: [p.characteristics[secondCharacteristicIndex]],
-                z: [p.characteristics[thirdCharacteristicIndex]],
+                x: p.fragmentsData.map(fd => fd.characteristics[firstCharacteristicIndex]),
+                y: p.fragmentsData.map(fd => fd.characteristics[secondCharacteristicIndex]),
+                z: p.fragmentsData.map(fd => fd.characteristics[thirdCharacteristicIndex]),
                 text: p.name,
                 mode: "markers",
-                marker: { opacity: 0.8, color: $scope.legend[p.legendIndex].color },
-                name: p.name,
+                marker: {
+                    opacity: 0.8,
+                    color: $scope.legend[p.legendIndex].color,
+                    size: 3
+                },
+                name: p.researchObjectName,
                 type: "scatter3d",
                 customdata: { legendId: p.legendId },
                 visible: $scope.legend[p.legendIndex].visible
@@ -277,12 +292,12 @@
                 //pad: [80, 80, 80, 80],
                 line: {
                     color: $scope.points.map(p => p.legendIndex),
-                    colorscale: "Turbo"
+                    colorscale: "Turbo",
                 },
 
                 dimensions: characteristicsIndices.map(ci => ({
                     label: $scope.characteristicNames[ci],
-                    values: $scope.points.map(p => p.characteristics[ci])
+                    values: $scope.points.map(p => p.fragmentsData.map(fd => fd.characteristics[ci]))
                 }))
             }];
 
@@ -302,7 +317,7 @@
 
             switch ($scope.chartCharacteristics.length) {
                 case 1:
-                    $scope.fillBarPlotData();
+                    $scope.fillLinePlotData();
                     break;
                 case 2:
                     $scope.fillScatterPlotData();
@@ -367,182 +382,10 @@
             });
         }
 
-        //function draw() {
-        //    $scope.fillPoints();
-
-        //    // removing previous chart and tooltip if any
-        //    d3.select(".chart-tooltip").remove();
-        //    d3.select(".chart-svg").remove();
-
-        //    // chart size and margin settings
-        //    let margin = { top: 30 + $scope.legendHeight, right: 30, bottom: 30, left: 60 };
-        //    let width = $scope.width - margin.left - margin.right;
-        //    let height = $scope.height - margin.top - margin.bottom;
-
-        //    // setup x
-        //    // calculating margins for dots
-        //    let xMin = d3.min($scope.points, $scope.xValue);
-        //    let xMax = d3.max($scope.points, $scope.xValue);
-        //    let xMargin = (xMax - xMin) * 0.05;
-
-        //    let xScale = d3.scaleLinear()
-        //        .domain([xMin - xMargin, xMax + xMargin])
-        //        .range([0, width]);
-        //    let xAxis = d3.axisBottom(xScale)
-        //        .tickSizeInner(-height)
-        //        .tickSizeOuter(0)
-        //        .tickPadding(10);
-
-        //    $scope.xMap = d => xScale($scope.xValue(d));
-
-        //    // setup y
-        //    // calculating margins for dots
-        //    let yMax = d3.max($scope.points, $scope.yValue);
-        //    let yMin = d3.min($scope.points, $scope.yValue);
-        //    let yMargin = (yMax - yMin) * 0.05;
-
-        //    let yScale = d3.scaleLinear()
-        //        .domain([yMin - yMargin, yMax + yMargin])
-        //        .range([height, 0]);
-        //    let yAxis = d3.axisLeft(yScale)
-        //        .tickSizeInner(-width)
-        //        .tickSizeOuter(0)
-        //        .tickPadding(10);
-
-        //    $scope.yMap = d => yScale($scope.yValue(d));
-
-        //    // setup fill color
-        //    let color = d3.scaleSequential(d3.interpolateTurbo).domain([0, $scope.legend.length]);
-
-        //    // add the graph canvas to the body of the webpage
-        //    let svg = d3.select("#chart").append("svg")
-        //        .attr("width", $scope.width)
-        //        .attr("height", $scope.height)
-        //        .attr("class", "chart-svg")
-        //        .append("g")
-        //        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        //    // add the tooltip area to the webpage
-        //    let tooltip = d3.select("#chart").append("div")
-        //        .attr("class", "chart-tooltip position-absolute bg-light font-monospace small lh-sm p-1 rounded")
-        //        .style("opacity", 0);
-
-        //    // preventing tooltip hiding if dot clicked
-        //    tooltip.on("click", () => { tooltip.hideTooltip = false; });
-
-        //    // hiding tooltip
-        //    d3.select("#chart").on("click", () => { $scope.clearTooltip(tooltip); });
-
-        //    // x-axis
-        //    svg.append("g")
-        //        .attr("class", "x axis")
-        //        .attr("transform", `translate(0,${height})`)
-        //        .call(xAxis);
-
-        //    svg.append("text")
-        //        .attr("class", "label")
-        //        .attr("transform", `translate(${width / 2} ,${height + margin.top - $scope.legendHeight})`)
-        //        .style("text-anchor", "middle")
-        //        .text($scope.lineChart ? "Fragment №" : $scope.firstCharacteristic.Text)
-        //        .style("font-size", "12pt");
-
-        //    // y-axis
-        //    svg.append("g")
-        //        .attr("class", "y axis")
-        //        .call(yAxis);
-
-        //    svg.append("text")
-        //        .attr("class", "label")
-        //        .attr("transform", "rotate(-90)")
-        //        .attr("y", 0 - margin.left)
-        //        .attr("x", 0 - (height / 2))
-        //        .attr("dy", ".71em")
-        //        .style("text-anchor", "middle")
-        //        .text($scope.lineChart ? $scope.firstCharacteristic.Text : $scope.secondCharacteristic.Text)
-        //        .style("font-size", "12pt");
-
-        //    if ($scope.lineChart) {
-        //        let line = d3.line()
-        //            .x($scope.xMap)
-        //            .y($scope.yMap);
-
-        //        // Group the entries by symbol
-        //        let dataGroups = d3.group($scope.points, d => d.researchObjectName);
-
-        //        // Loop through each symbol / key
-        //        dataGroups.forEach(value => {
-        //            svg.append("path")
-        //                .datum(value)
-        //                .attr("class", "line")
-        //                .attr("d", line)
-        //                .attr("stroke", d => color(d[0].characteristicId))
-        //                .attr("stroke-width", 1)
-        //                .attr("fill", "none");
-        //        });
-        //    }
-
-        //    // draw dots
-        //    svg.selectAll(".dot")
-        //        .data($scope.points)
-        //        .enter()
-        //        .append("ellipse")
-        //        .attr("class", "dot")
-        //        .attr("rx", $scope.dotRadius)
-        //        .attr("ry", $scope.dotRadius)
-        //        .attr("cx", $scope.xMap)
-        //        .attr("cy", $scope.yMap)
-        //        .style("fill-opacity", 0.6)
-        //        .style("opacity", $scope.lineChart ? 0 : 1)
-        //        .style("fill", d => color(d.characteristicId))
-        //        .style("stroke", d => color(d.characteristicId))
-        //        .on("click", (event, d) => $scope.showTooltip(event, d, tooltip, svg));
-
-        //    // draw legend
-        //    let legend = svg.selectAll(".legend")
-        //        .data($scope.legend)
-        //        .enter()
-        //        .append("g")
-        //        .attr("class", "legend")
-        //        .attr("transform", (_d, i) => `translate(0,${i * 20})`)
-        //        .on("click", function (event, d) {
-        //            d.visible = !d.visible;
-        //            let legendEntry = d3.select(event.currentTarget);
-        //            legendEntry.select("text")
-        //                .style("opacity", () => d.visible ? 1 : 0.5);
-        //            legendEntry.select("rect")
-        //                .style("fill-opacity", () => d.visible ? 1 : 0);
-
-        //            svg.selectAll(".dot")
-        //                .filter(dot => dot.researchObjectName === d.name)
-        //                .attr("visibility", () => d.visible ? "visible" : "hidden");
-
-        //            svg.selectAll(".line")
-        //                .filter(line => line[0].researchObjectName === d.name)
-        //                .attr("visibility", () => d.visible ? "visible" : "hidden");
-        //        });
-
-        //    // draw legend colored rectangles
-        //    legend.append("rect")
-        //        .attr("width", 15)
-        //        .attr("height", 15)
-        //        .style("fill", d => color(d.id))
-        //        .style("stroke", d => color(d.id))
-        //        .style("stroke-width", 4)
-        //        .attr("transform", `translate(0, -${$scope.legendHeight})`);
-
-        //    // draw legend text
-        //    legend.append("text")
-        //        .attr("x", 24)
-        //        .attr("y", 9)
-        //        .attr("dy", ".35em")
-        //        .attr("transform", `translate(0, -${$scope.legendHeight})`)
-        //        .text(d => d.name)
-        //        .style("font-size", "9pt");
-        //}
 
         $scope.calculateLocalCharacteristicsSimilarityMatrix = calculateLocalCharacteristicsSimilarityMatrix;
         $scope.changeCharacteristicsTableVisibility = changeCharacteristicsTableVisibility;
-        $scope.fillBarPlotData = fillBarPlotData;
+        $scope.fillLinePlotData = fillLinePlotData;
         $scope.fillScatterPlotData = fillScatterPlotData;
         $scope.fill3dScatterPlotData = fill3dScatterPlotData;
         $scope.fillParallelCoordinatesPlotData = fillParallelCoordinatesPlotData;
