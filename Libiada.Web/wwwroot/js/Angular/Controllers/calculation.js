18dfae0057e57cc2e134e0936cbac9d56103d9b2
Andrey Karetnikov <andrew.karetnikov@gmail.com>
2025-04-23 18:01:36 +0600

    Added CalculationControllerClass into calculation.ts (file js - correctly)

--- Libiada.Web/wwwroot/js/Angular/Controllers/calculation.js
+++ Libiada.Web/wwwroot/js/Angular/Controllers/calculation.js
@@ -1,43 +1,60 @@
-function CalculationController(data) {
-    "use strict";
-    function calculation($scope, filterFilter) {
-        MapModelFromJson($scope, data);
-        function filterByNature() {
-            if (!$scope.hideNotation) {
-                $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
-                // if notation is not linked to characteristic
-                angular.forEach($scope.characteristics, (characteristic) => {
-                    characteristic.notation = $scope.notation; // Исправлено: notation -> $scope.notation
-                });
-            }
-        }
-        function setUnselectAllResearchObjectsFunction(func) {
-            $scope.unselectAllResearchObjects = func;
-        }
-        function setUnselectAllSequenceGroupsFunction(func) {
-            $scope.unselectAllSequenceGroups = func;
-        }
-        function clearSelection() {
-            if ($scope.unselectAllResearchObjects)
-                $scope.unselectAllResearchObjects();
-            if ($scope.unselectAllSequenceGroups)
-                $scope.unselectAllSequenceGroups();
-        }
-        $scope.filterByNature = filterByNature;
-        $scope.setUnselectAllResearchObjectsFunction = setUnselectAllResearchObjectsFunction;
-        $scope.setUnselectAllSequenceGroupsFunction = setUnselectAllSequenceGroupsFunction;
-        $scope.clearSelection = clearSelection;
-        // if notation is not linked to characteristic
-        $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
-        $scope.language = $scope.languages?.[0];
-        $scope.translator = $scope.translators?.[0];
-        $scope.pauseTreatment = $scope.pauseTreatment ?? $scope.pauseTreatments?.[0]; // Заменено оператора ??= на стандартный ??
-        $scope.calculaionFor = "researchObjects";
-        // if we are in clusterization
-        if ($scope.ClusterizatorsTypes) {
-            $scope.ClusterizationType = $scope.ClusterizatorsTypes[0];
-        }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//declare var angular: any;
+//import * as angular from "../lib/angular.js/angular.js";
+var angular_1 = require("angular");
+var CalculationControllerClass = /** @class */ (function () {
+    function CalculationControllerClass(data) {
+        this.data = data;
+        this.initializeController();
     }
-    angular.module("libiada").controller("CalculationCtrl", ["$scope", "filterFilter", calculation]);
+    CalculationControllerClass.prototype.initializeController = function () {
+        "use strict";
+        var _this = this;
+        var calculation = function ($scope, filterFilter) {
+            var _a, _b, _c, _d;
+            MapModelFromJson($scope, _this.data);
+            function filterByNature() {
+                if (!$scope.hideNotation) {
+                    $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
+                    // if notation is not linked to characteristic
+                    angular_1.default.forEach($scope.characteristics, function (characteristic) {
+                        characteristic.notation = $scope.notation;
+                    });
+                }
+            }
+            function setUnselectAllResearchObjectsFunction(func) {
+                $scope.unselectAllResearchObjects = func;
+            }
+            function setUnselectAllSequenceGroupsFunction(func) {
+                $scope.unselectAllSequenceGroups = func;
+            }
+            function clearSelection() {
+                if ($scope.unselectAllResearchObjects)
+                    $scope.unselectAllResearchObjects();
+                if ($scope.unselectAllSequenceGroups)
+                    $scope.unselectAllSequenceGroups();
+            }
+            $scope.filterByNature = filterByNature;
+            $scope.setUnselectAllResearchObjectsFunction = setUnselectAllResearchObjectsFunction;
+            $scope.setUnselectAllSequenceGroupsFunction = setUnselectAllSequenceGroupsFunction;
+            $scope.clearSelection = clearSelection;
+            // if notation is not linked to characteristic
+            $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
+            $scope.language = (_a = $scope.languages) === null || _a === void 0 ? void 0 : _a[0];
+            $scope.translator = (_b = $scope.translators) === null || _b === void 0 ? void 0 : _b[0];
+            $scope.pauseTreatment = (_c = $scope.pauseTreatment) !== null && _c !== void 0 ? _c : (_d = $scope.pauseTreatments) === null || _d === void 0 ? void 0 : _d[0];
+            $scope.calculaionFor = "researchObjects";
+            // if we are in clusterization
+            if ($scope.ClusterizatorsTypes) {
+                $scope.ClusterizationType = $scope.ClusterizatorsTypes[0];
+            }
+        };
+        angular_1.default.module("libiada").controller("CalculationCtrl", ["$scope", "filterFilter", calculation]);
+    };
+    return CalculationControllerClass;
+}());
+// Функция-обертка для обратной совместимости
+function CalculationController(data) {
+    return new CalculationControllerClass(data);
 }
-//# sourceMappingURL=calculation.js.map
--- Libiada.Web/wwwroot/js/Angular/Controllers/calculation.ts
+++ Libiada.Web/wwwroot/js/Angular/Controllers/calculation.ts
@@ -1,55 +1,71 @@
 ﻿declare var angular: any;
+//import * as angular from "../lib/angular.js/angular.js";
+
 
 // Объявляем существующую JavaScript функцию
 declare function MapModelFromJson($scope: any, data: any): void;
 
-function CalculationController(data: any) {
-    "use strict";
+class CalculationControllerClass {
+    private data: any;
 
-    function calculation($scope: any, filterFilter: any): void {
-        MapModelFromJson($scope, data);
-
-        function filterByNature(): void {
-            if (!$scope.hideNotation) {
-                $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
-
-                // if notation is not linked to characteristic
-                angular.forEach($scope.characteristics, (characteristic: any) => {
-                    characteristic.notation = $scope.notation; // Исправлено: notation -> $scope.notation
-                });
-            }
-        }
-
-        function setUnselectAllResearchObjectsFunction(func: Function): void {
-            $scope.unselectAllResearchObjects = func;
-        }
-
-        function setUnselectAllSequenceGroupsFunction(func: Function): void {
-            $scope.unselectAllSequenceGroups = func;
-        }
-
-        function clearSelection(): void {
-            if ($scope.unselectAllResearchObjects) $scope.unselectAllResearchObjects();
-            if ($scope.unselectAllSequenceGroups) $scope.unselectAllSequenceGroups();
-        }
-
-        $scope.filterByNature = filterByNature;
-        $scope.setUnselectAllResearchObjectsFunction = setUnselectAllResearchObjectsFunction;
-        $scope.setUnselectAllSequenceGroupsFunction = setUnselectAllSequenceGroupsFunction;
-        $scope.clearSelection = clearSelection;
-
-        // if notation is not linked to characteristic
-        $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
-        $scope.language = $scope.languages?.[0];
-        $scope.translator = $scope.translators?.[0];
-        $scope.pauseTreatment = $scope.pauseTreatment ?? $scope.pauseTreatments?.[0]; // Заменено оператора ??= на стандартный ??
-        $scope.calculaionFor = "researchObjects";
-
-        // if we are in clusterization
-        if ($scope.ClusterizatorsTypes) {
-            $scope.ClusterizationType = $scope.ClusterizatorsTypes[0];
-        }
+    constructor(data: any) {
+        this.data = data;
+        this.initializeController();
     }
 
-    angular.module("libiada").controller("CalculationCtrl", ["$scope", "filterFilter", calculation]);
+    private initializeController(): void {
+        "use strict";
+
+        const calculation = ($scope: any, filterFilter: any): void => {
+            MapModelFromJson($scope, this.data);
+
+            function filterByNature(): void {
+                if (!$scope.hideNotation) {
+                    $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
+
+                    // if notation is not linked to characteristic
+                    angular.forEach($scope.characteristics, (characteristic: any) => {
+                        characteristic.notation = $scope.notation;
+                    });
+                }
+            }
+
+            function setUnselectAllResearchObjectsFunction(func: Function): void {
+                $scope.unselectAllResearchObjects = func;
+            }
+
+            function setUnselectAllSequenceGroupsFunction(func: Function): void {
+                $scope.unselectAllSequenceGroups = func;
+            }
+
+            function clearSelection(): void {
+                if ($scope.unselectAllResearchObjects) $scope.unselectAllResearchObjects();
+                if ($scope.unselectAllSequenceGroups) $scope.unselectAllSequenceGroups();
+            }
+
+            $scope.filterByNature = filterByNature;
+            $scope.setUnselectAllResearchObjectsFunction = setUnselectAllResearchObjectsFunction;
+            $scope.setUnselectAllSequenceGroupsFunction = setUnselectAllSequenceGroupsFunction;
+            $scope.clearSelection = clearSelection;
+
+            // if notation is not linked to characteristic
+            $scope.notation = filterFilter($scope.notations, { Nature: $scope.nature })[0];
+            $scope.language = $scope.languages?.[0];
+            $scope.translator = $scope.translators?.[0];
+            $scope.pauseTreatment = $scope.pauseTreatment ?? $scope.pauseTreatments?.[0];
+            $scope.calculaionFor = "researchObjects";
+
+            // if we are in clusterization
+            if ($scope.ClusterizatorsTypes) {
+                $scope.ClusterizationType = $scope.ClusterizatorsTypes[0];
+            }
+        };
+
+        angular.module("libiada").controller("CalculationCtrl", ["$scope", "filterFilter", calculation]);
+    }
+}
+
+// Функция-обертка для обратной совместимости
+function CalculationController(data: any) {
+    return new CalculationControllerClass(data);
 }
