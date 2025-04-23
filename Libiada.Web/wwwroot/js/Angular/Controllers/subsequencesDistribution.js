f8e01f32634f495978f8dcec1acab1bb9a7fe5f0
Andrey Karetnikov <andrew.karetnikov@gmail.com>
2025-04-23 22:55:56 +0600

    Resolved conflicts in tsconfig.json; modified .gitignore (# Ignore all .js.map files)

--- Libiada.Web/wwwroot/js/Angular/Controllers/subsequencesDistribution.js
+++ Libiada.Web/wwwroot/js/Angular/Controllers/subsequencesDistribution.js
@@ -1,23 +1,22 @@
 // Основной класс контроллера
-var SubsequencesDistributionController = /** @class */ (function () {
-    function SubsequencesDistributionController(data) {
+class SubsequencesDistributionController {
+    constructor(data) {
         this.data = data;
         this.initialize();
     }
-    SubsequencesDistributionController.prototype.initialize = function () {
-        var _this = this;
+    initialize() {
         // Определение функции контроллера
-        var subsequencesDistribution = function ($scope) {
-            MapModelFromJson($scope, _this.data);
+        const subsequencesDistribution = ($scope) => {
+            MapModelFromJson($scope, this.data);
         };
         // Регистрация контроллера в Angular модуле
         angular.module("libiada")
             .controller("SubsequencesDistributionCtrl", ["$scope", subsequencesDistribution]);
-    };
-    return SubsequencesDistributionController;
-}());
+    }
+}
 // Экспорт конструктора для использования в _AngularControllerInitializer.cshtml
 window.SubsequencesDistributionController = function (data) {
     "use strict";
     new SubsequencesDistributionController(data);
 };
+//# sourceMappingURL=subsequencesDistribution.js.map
